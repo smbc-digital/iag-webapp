ba895a4eff2c0ca0b5a6cbdc93ad102f0e56eabc
Jon Crawford <jonathan.crawford@stockport.gov.uk>
2016-11-14 11:54:58 +0000

    Neil/JonCrawf - removed title redesign feature toggle

--- src/StockportWebapp/FeatureToggling/FeatureToggles.cs
+++ src/StockportWebapp/FeatureToggling/FeatureToggles.cs
@@ -3,7 +3,6 @@ namespace StockportWebapp.FeatureToggling
     public class FeatureToggles
     {
         public bool DynamicContactUsForm { get; set; } = false;
-        public bool TitleRedesign { get; set; } = false;
         public bool BusinessIdFromRequest { get; set; } = false;
         public bool LiveChat { get; set; } = false;
         public bool NewsCategory { get; set; } = false;
