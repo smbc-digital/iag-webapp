bc50f4bd56bc8a20e5140ea487075344d76d9dde
Raj Paili <raj.paili@stockport.gov.uk>
2016-11-16 14:54:23 +0000

    Raj/Dan/Neil #624 Improve routes tests speed
    Using single test server startup where configuration not changing.
    There may be other tests where restart not needed, but already this
    gives good build perf improvement.

---
+++ test/StockportWebappTests/Integration/RoutesTestServerFixture.cs
@@ -0,0 +1,35 @@
+using System;
+using Microsoft.AspNetCore.TestHost;
+using HttpClient = System.Net.Http.HttpClient;
+
+namespace StockportWebappTests.Integration
+{
+    public class RoutesTestServerFixture : IDisposable
+    {
+        private readonly HttpClient _client;
+        private readonly TestServer _server;
+        private const string IntEnvironment = "int";
+
+        public RoutesTestServerFixture()
+        {
+            TestContentApiFixture.SetupContentApiResponses();
+
+            _server = TestAppFactory.MakeFakeApp("healthystockport", IntEnvironment);
+            _client = _server.CreateClient();
+        }
+
+        public HttpClient Client => _client;
+
+        public void SetBusinessIdRequestHeader(string business)
+        {
+            _client.DefaultRequestHeaders.Remove("BUSINESS-ID");
+            _client.DefaultRequestHeaders.Add("BUSINESS-ID", business);
+        }
+
+        public void Dispose()
+        {
+            _client.Dispose();
+            _server.Dispose();
+        }
+    }
+}
