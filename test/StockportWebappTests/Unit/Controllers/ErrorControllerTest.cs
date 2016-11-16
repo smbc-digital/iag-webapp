59d764a1d32c993e3875080b04b5a20e6e9828aa
Raj Paili <raj.paili@stockport.gov.uk>
2016-11-16 16:03:36 +0000

    Raj/Dan #624 Added missing coverage for error controller

---
+++ test/StockportWebappTests/Unit/Controllers/ErrorControllerTest.cs
@@ -0,0 +1,33 @@
+﻿using FluentAssertions;
+using Microsoft.AspNetCore.Mvc;
+using StockportWebapp.Controllers;
+using Xunit;
+
+namespace StockportWebappTests.Unit.Controllers
+{
+    public class ErrorControllerTest
+    {
+        private readonly ErrorController _controller;
+
+        public ErrorControllerTest()
+        {
+            _controller = new ErrorController();
+        }
+
+        [Fact]
+        public void ShouldTellUsSomethingsMissingIfAPageWasNotFound()
+        {
+            var result = AsyncTestHelper.Resolve(_controller.Error("404")) as ViewResult;
+
+            result.ViewData[@"ErrorHeading"].Should().Be("Something's missing");
+        }
+
+        [Fact]
+        public void ShouldTellUsSomethingIsWrongIfADifferentErrorOccurred()
+        {
+            var result = AsyncTestHelper.Resolve(_controller.Error("500")) as ViewResult;
+
+            result.ViewData[@"ErrorHeading"].Should().Be("Something went wrong");
+        }
+    }
+}
