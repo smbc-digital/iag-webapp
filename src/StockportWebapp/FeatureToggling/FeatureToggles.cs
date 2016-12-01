67ab2be65be05d201217c0a9d68eb4019220b0af
Neil Redfern <neil.redfern@stockport.gov.uk>
2016-12-01 14:20:38 +0000

    Neil - changed pull-right to use unsemantic

--- src/StockportWebapp/FeatureToggling/FeatureToggles.cs
+++ src/StockportWebapp/FeatureToggling/FeatureToggles.cs
@@ -6,5 +6,6 @@ namespace StockportWebapp.FeatureToggling
         public bool LiveChat { get; set; } = false;
         public bool LegacyUrlRedirects { get; set; } = false;
         public bool Search { get; set; } = false;
+        public bool BetaToWwwRedirect { get; set; } = false;
     }
 }
