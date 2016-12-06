1671d4251098852f30d0a6274d99d687978fe152
Neil Redfern <neil.redfern@stockport.gov.uk>
2016-12-06 11:26:51 +0000

    Neil/Jon #585 - news listing refactor

--- src/StockportWebapp/wwwroot/assets/javascript/stockportgov/newsroom.js
+++ src/StockportWebapp/wwwroot/assets/javascript/stockportgov/newsroom.js
@@ -7,18 +7,6 @@
         }
     });
 
-var matchboxNews = new Matchbox({
-    parentSelector: '.nav-card-news-list',
-    childSelector: '.nav-card-news',
-    groupsOf: 1,
-    breakpoints: [
-    { bp: 767, groupsOf: 2 },
-    { bp: 1024, groupsOf: 3 }
-    ]
-});
-
-if ($(".nav-card-news-list").length) { matchboxNews.init(); }
-
 $(".l-news-filter .filter-title, .l-news-filter .filter-inner-title").click(function () {
     $(this).siblings("ul").slideToggle(100, function() {
         $(this).parent().toggleClass("is-collapsed");
