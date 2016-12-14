cbe648cf83a7702e076c79651566287bccc9f131
danm <writetodan@yahoo.com>
2016-12-14 22:35:44 +0000

    Dan #660 Test for tag helper rendered in context
    Although from file at mo... would be better passed in as string
    rather than from .cshtml that is not inline in test

---
+++ test/StockportWebappTests/Unit/Views/TagHelperTest.cs
@@ -0,0 +1,29 @@
+﻿
+using FluentAssertions;
+using Xunit;
+
+using StockportWebapp.Models;
+
+namespace StockportWebappTests.Unit.Views
+{
+    public class TagHelperTest
+    {
+        private TestViewRenderer renderer;
+
+        public TagHelperTest()
+        {
+            renderer = new TestViewRenderer("test", "StockportWebappTests");
+        }
+
+        [Fact]
+        public void RendersARazorTemplate()
+        { 
+            var html = renderer.RenderView("TestTemplates/StockButtons", "");
+
+            html.Should().Contain("the button");
+            html.Should().Contain("<button");
+            html.Should().NotContain("stock-button-submit");
+        }
+    }
+
+}
