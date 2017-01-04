6aa0428d19fd70a1eb06abb196ae5eccddf34013
Raj Paili <raj.paili@stockport.gov.uk>
2017-01-04 15:42:32 +0000

    #691 Clare/Raj Added new resources to CSP header

--- src/StockportWebapp/Middleware/SecurityHeaderMiddleware.cs
+++ src/StockportWebapp/Middleware/SecurityHeaderMiddleware.cs
@@ -44,9 +44,9 @@ namespace StockportWebapp.Middleware
             {
                 {"default-src", "'self'"},
                 {"font-src", "'self' font.googleapis.com maxcdn.bootstrapcdn.com/font-awesome/ fonts.gstatic.com/"},
-                {"img-src", "'self' images.contentful.com/ s3-eu-west-1.amazonaws.com/live-iag-static-assets/ uk1.siteimprove.com/ stockportb.logo-net.co.uk/ .cloudfront.net/butotv/"},
-                {"style-src", "'self' 'unsafe-inline' *.cludo.com/css/ maxcdn.bootstrapcdn.com/font-awesome/ fonts.googleapis.com/ cloudfront.net/butotv/"},
-                {"script-src", "'self' 'unsafe-inline' ajax.googleapis.com/ajax/libs/jquery/  *.cludo.com/ cdnjs.cloudflare.com/ajax/libs/cookieconsent2/ s3-eu-west-1.amazonaws.com/share.typeform.com/ js.buto.tv/video/ s7.addthis.com/js/300/addthis_widget.js siteimproveanalytics.com/js/ *.logo-net.co.uk/Delivery/"},
+                {"img-src", "'self' images.contentful.com/ s3-eu-west-1.amazonaws.com/live-iag-static-assets/ uk1.siteimprove.com/ stockportb.logo-net.co.uk/ *.cloudfront.net/butotv/"},
+                {"style-src", "'self' 'unsafe-inline' *.cludo.com/css/ maxcdn.bootstrapcdn.com/font-awesome/ fonts.googleapis.com/ *.cloudfront.net/butotv/"},
+                {"script-src", "'self' 'unsafe-inline' ajax.googleapis.com/ajax/libs/jquery/ *.cludo.com/ cdnjs.cloudflare.com/ajax/libs/cookieconsent2/ s3-eu-west-1.amazonaws.com/share.typeform.com/ js.buto.tv/video/ s7.addthis.com/js/300/addthis_widget.js siteimproveanalytics.com/js/ *.logo-net.co.uk/Delivery/"},
                 {"connect-src", "'self' https://api.cludo.com/ buto-ping-middleman.buto.tv/" },
                 {"media-src", "'self' https://www.youtube.com/ *.cloudfront.net/butotv/live/videos/" },
                 {"object-src", "'self'  https://www.youtube.com http://www.youtube.com" }
