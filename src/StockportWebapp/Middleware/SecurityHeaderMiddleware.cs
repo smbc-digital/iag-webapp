9159b1705ab22021924dd06014a7d4a5a1e7b593
Jonathan Hadley <jonathan.hadley@stockport.gov.uk>
2017-01-04 09:50:01 +0000

    JonH/Raj Updates Security Headers

--- src/StockportWebapp/Middleware/SecurityHeaderMiddleware.cs
+++ src/StockportWebapp/Middleware/SecurityHeaderMiddleware.cs
@@ -25,12 +25,35 @@ namespace StockportWebapp.Middleware
             var host = httpContext.Request.Host.Value.ToLower();
             var isRemoteHost = host.StartsWith("www") || host.StartsWith("int-") || host.StartsWith("qa-") || host.StartsWith("stage-");
 
+            if (_featureToggles.SecurityHeaders)
+            {
+                httpContext.Response.Headers.Add("Content-Security-Policy", new[] { GetContentSecurityHeaderValue() });
+            }
+
             if (_featureToggles.SecurityHeaders && isRemoteHost)
             {
                 httpContext.Response.Headers.Add("Strict-Transport-Security", new[] { "max-age=31536000" });
             }
 
             return _next(httpContext);
+        }
+
+        private static string GetContentSecurityHeaderValue()
+        {
+            var cspHeaders = new Dictionary<string, string>()
+            {
+                {"default-src", "https:"},
+                {"font-src", "'self' https://font.googleapis.com"},
+                {"img-src", "'self' https://images.contentful.com https://s3-eu-west-1.amazonaws.com/live-iag-static-assets/"},
+                {"style-src", "'self' 'unsafe-inline' https://customer.cludo.com/css/112/1144/ https://maxcdn.bootstrapcdn.com/font-awesome/"},
+                {"script-src", "'self' 'unsafe-inline' https://ajax.googleapis.com/ajax/libs/jquery/ https://api.cludo.com/scripts/ https://cdnjs.cloudflare.com/ajax/libs/cookieconsent2/ https://s3-eu-west-1.amazonaws.com/share.typeform.com/ https://js.buto.tv/video/ https://s7.addthis.com/js/300/addthis_widget.js"},
+                {"form-action", "'self'" },
+                {"media-src", "'self' https://www.youtube.com/ *.cloudfront.net/butotv/live/videos/" }
+            };
+
+            var headerValue = string.Join("; ", cspHeaders.Select(x => $"{x.Key} {x.Value}"));
+
+            return headerValue;
         }
     }
 }
--- test/StockportWebappTests/Unit/Middleware/SecurityHeaderMiddlewareTests.cs
+++ test/StockportWebappTests/Unit/Middleware/SecurityHeaderMiddlewareTests.cs
@@ -84,5 +84,36 @@ namespace StockportWebappTests.Unit.Middleware
 
             context.Response.Headers["Strict-Transport-Security"].ToString().Should().BeNullOrEmpty();
         }
+
+        [Fact]
+        public void ShouldContainContentSecurityPolicyHeader()
+        {
+            var context = new DefaultHttpContext();
+            context.Request.Host = new HostString("www.domain.com");
+
+            _middleware.Invoke(context);
+
+            var header = context.Response.Headers["Content-Security-Policy"].ToString();
+
+            header.Should().NotBeNullOrEmpty();
+            header.Should().Contain("default-src https:");
+        }
+
+        [Fact]
+        public void ShouldContainContentSecurityPolicyHeaderForSrcReferences()
+        {
+            var context = new DefaultHttpContext();
+            context.Request.Host = new HostString("www.domain.com");
+
+            _middleware.Invoke(context);
+            var header = context.Response.Headers["Content-Security-Policy"].ToString();
+
+            header.Should().Contain("font-src 'self' https://font.googleapis.com");
+            header.Should().Contain("img-src 'self' https://images.contentful.com https://s3-eu-west-1.amazonaws.com/live-iag-static-assets/");
+            header.Should().Contain("style-src 'self' 'unsafe-inline' https://customer.cludo.com/css/112/1144/ https://maxcdn.bootstrapcdn.com/font-awesome/");
+            header.Should().Contain("script-src 'self' 'unsafe-inline' https://ajax.googleapis.com/ajax/libs/jquery/ https://api.cludo.com/scripts/ https://cdnjs.cloudflare.com/ajax/libs/cookieconsent2/ https://s3-eu-west-1.amazonaws.com/share.typeform.com/ https://js.buto.tv/video/ https://s7.addthis.com/js/300/addthis_widget.js");
+            header.Should().Contain("form-action 'self'");
+            header.Should().Contain("media-src 'self' https://www.youtube.com/ *.cloudfront.net/butotv/live/videos/");
+        }
     }
 }
