d5ff1e3b5da8e45d40c61d5f7e87757e0a44bb9c
Raj Paili <raj.paili@stockport.gov.uk>
2017-01-06 15:08:16 +0000

    Raj/Jon Implemented Subresource Integrity and modified Content Security header

--- src/StockportWebapp/Middleware/SecurityHeaderMiddleware.cs
+++ src/StockportWebapp/Middleware/SecurityHeaderMiddleware.cs
@@ -1,6 +1,4 @@
-﻿using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+﻿using System.Threading.Tasks;
 using Microsoft.AspNetCore.Http;
 using StockportWebapp.FeatureToggling;
 
@@ -22,36 +20,12 @@ namespace StockportWebapp.Middleware
             var host = httpContext.Request.Host.Value.ToLower();
             var isRemoteHost = host.StartsWith("www") || host.StartsWith("int-") || host.StartsWith("qa-") || host.StartsWith("stage-");
 
-            if (_featureToggles.SecurityHeaders)
-            {
-                httpContext.Response.Headers.Add("Content-Security-Policy", new[] { GetContentSecurityHeaderValue() });
-            }
-
             if (_featureToggles.SecurityHeaders && isRemoteHost)
             {
                 httpContext.Response.Headers.Add("Strict-Transport-Security", new[] { "max-age=31536000" });
             }
 
             return _next(httpContext);
-        }
-
-        private static string GetContentSecurityHeaderValue()
-        {
-            var cspHeaders = new Dictionary<string, string>()
-            {
-                {"default-src", "http:"},
-                {"font-src", "'self' font.googleapis.com maxcdn.bootstrapcdn.com/font-awesome/ fonts.gstatic.com/"},
-                {"img-src", "'self' images.contentful.com/ s3-eu-west-1.amazonaws.com/live-iag-static-assets/ uk1.siteimprove.com/ stockportb.logo-net.co.uk/ *.cloudfront.net/butotv/"},
-                {"style-src", "'self' 'unsafe-inline' *.cludo.com/css/ maxcdn.bootstrapcdn.com/font-awesome/ fonts.googleapis.com/ *.cloudfront.net/butotv/"},
-                {"script-src", "'self' 'unsafe-inline' 'unsafe-eval' https://ajax.googleapis.com/ajax/libs/jquery/ api.cludo.com/scripts/ customer.cludo.com/scripts/ cdnjs.cloudflare.com/ajax/libs/cookieconsent2/ s3-eu-west-1.amazonaws.com/share.typeform.com/ js.buto.tv/video/ s7.addthis.com/js/300/addthis_widget.js m.addthis.com/live/ siteimproveanalytics.com/js/ *.logo-net.co.uk/Delivery/"},
-                {"connect-src", "'self' https://api.cludo.com/ buto-ping-middleman.buto.tv/ m.addthis.com/live/" },
-                {"media-src", "'self' https://www.youtube.com/ *.cloudfront.net/butotv/live/videos/" },
-                {"object-src", "'self'  https://www.youtube.com http://www.youtube.com" }
-            };
-
-            var headerValue = string.Join("; ", cspHeaders.Select(x => $"{x.Key} {x.Value}"));
-
-            return headerValue;
         }
     }
 }
--- test/StockportWebappTests/Unit/Middleware/SecurityHeaderMiddlewareTest.cs
+++ test/StockportWebappTests/Unit/Middleware/SecurityHeaderMiddlewareTest.cs
@@ -80,23 +80,5 @@ namespace StockportWebappTests.Unit.Middleware
 
             context.Response.Headers["Strict-Transport-Security"].ToString().Should().BeNullOrEmpty();
         }
-
-        [Fact]
-        public void ShouldContainContentSecurityPolicyHeaderForSrcReferences()
-        {
-            var context = new DefaultHttpContext();
-            context.Request.Host = new HostString("www.domain.com");
-
-            _middleware.Invoke(context);
-            var header = context.Response.Headers["Content-Security-Policy"].ToString();
-
-            header.Should().Contain("font-src", "'self' font.googleapis.com maxcdn.bootstrapcdn.com/font-awesome/ fonts.gstatic.com/");
-            header.Should().Contain("img-src", "'self' images.contentful.com/ s3-eu-west-1.amazonaws.com/live-iag-static-assets/ uk1.siteimprove.com/ stockportb.logo-net.co.uk/ *.cloudfront.net/butotv/");
-            header.Should().Contain("style-src", "'self' 'unsafe-inline' *.cludo.com/css/ maxcdn.bootstrapcdn.com/font-awesome/ fonts.googleapis.com/ *.cloudfront.net/butotv/");
-            header.Should().Contain("script-src", "'self' 'unsafe-inline' 'unsafe-eval' https://ajax.googleapis.com/ajax/libs/jquery/ api.cludo.com/scripts/ customer.cludo.com/scripts/ cdnjs.cloudflare.com/ajax/libs/cookieconsent2/ s3-eu-west-1.amazonaws.com/share.typeform.com/ js.buto.tv/video/ s7.addthis.com/js/300/addthis_widget.js m.addthis.com/live/ siteimproveanalytics.com/js/ *.logo-net.co.uk/Delivery/");
-            header.Should().Contain("connect-src", "'self' https://api.cludo.com/ buto-ping-middleman.buto.tv/ m.addthis.com/live/");
-            header.Should().Contain("media-src", "'self' https://www.youtube.com/ *.cloudfront.net/butotv/live/videos/");
-            header.Should().Contain("object-src", "'self'  https://www.youtube.com http://www.youtube.com");
-        }
     }
 }
