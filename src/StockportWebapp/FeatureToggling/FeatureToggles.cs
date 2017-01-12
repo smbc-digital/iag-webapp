b892f9618c03c9d58be89838f7841ba3d4ed5308
Jon Crawford <jonathan.crawford@stockport.gov.uk>
2017-01-12 15:29:12 +0000

    Jon Craw / Jon H - #FeatureToggles - Removes live chat feature toggles

--- src/StockportWebapp/FeatureToggling/FeatureToggles.cs
+++ src/StockportWebapp/FeatureToggling/FeatureToggles.cs
@@ -3,7 +3,6 @@ namespace StockportWebapp.FeatureToggling
     public class FeatureToggles
     {
         public bool DynamicContactUsForm { get; set; } = false;
-        public bool LiveChat { get; set; } = false;
         public bool Search { get; set; } = false;
         public bool EventCalendar { get; set; } = false;
         public bool SecurityHeaders { get; set; } = false;
