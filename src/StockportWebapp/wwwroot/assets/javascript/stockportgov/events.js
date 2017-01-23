764775c7ece3d859f0911e020647677972effbdd
Neil Redfern <neil.redfern@stockport.gov.uk>
2017-01-23 11:34:43 +0000

    Neil #603 - added thank you page styles to events

--- src/StockportWebapp/wwwroot/assets/javascript/stockportgov/events.js
+++ src/StockportWebapp/wwwroot/assets/javascript/stockportgov/events.js
@@ -1,4 +1,6 @@
-﻿$(document).ready(function() {
+﻿$(document).ready(function () {
+    $(".schedule_multiple_events_inputs").hide();
+    $(".schedule_multiple_events").show();
     $(".schedule_multiple_events").click(function () {
         $(".schedule_multiple_events_inputs").slideToggle(200);
     });
