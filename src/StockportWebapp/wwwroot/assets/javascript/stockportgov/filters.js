83de5172598d50f01c81d9ead57703a790844f98
Raj Paili <raj.paili@stockport.gov.uk>
2017-02-07 10:51:56 +0000

    David/Raj #739 Added custom date filter in events

--- src/StockportWebapp/wwwroot/assets/javascript/stockportgov/filters.js
+++ src/StockportWebapp/wwwroot/assets/javascript/stockportgov/filters.js
@@ -3,3 +3,29 @@
         $(this).parent("li").toggleClass("is-collapsed");
     });
 });
+
+if ($("#custom-filter-li").hasClass("customdateactive")) {
+    $("#custom-filter-li").removeClass().addClass("customdateactive").addClass("collapsible");
+}
+else
+{
+    $("#custom-filter-li").removeClass().addClass("filter").addClass("is-collapsed");
+}
+
+$(function () {
+    $("#datepickerfrom").datepicker({
+        minDate: new Date(),
+        dateFormat: 'yy-mm-dd',
+        onClose: function (selectedDate) {
+            $("#datepickerto").datepicker("option", "minDate", selectedDate);
+        }
+    });
+
+    $("#datepickerto").datepicker({
+        dateFormat: 'yy-mm-dd',
+        onClose: function (selectedDate) {
+            $("#datepickerfrom").datepicker("option", "maxDate", selectedDate);
+        }
+    });
+});
+
