1cc1ed3dd1c898a7acb9ba3ebebf40339044a6ac
Raj Paili <raj.paili@stockport.gov.uk>
2017-02-08 12:44:36 +0000

    Raj/David #739 - Added feature toggle for events custom date filter

--- src/StockportWebapp/FeatureToggling/FeatureToggles.cs
+++ src/StockportWebapp/FeatureToggling/FeatureToggles.cs
@@ -3,5 +3,6 @@ namespace StockportWebapp.FeatureToggling
     public class FeatureToggles
     {      
         public bool FooterCache { get; set; } = false;
+        public bool EventCustomDateSearch { get; set; } = false;
     }
 }
