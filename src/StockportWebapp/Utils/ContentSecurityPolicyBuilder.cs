f2d052d9fdc7e4c098a92211d9a5d117cfb72323
SCN\clare.sudbery <clare.sudbery@thoughtworks.gov.uk>
2017-02-15 10:54:56 +0000

    David/Clare #780 Tidy up - get rid of commented out urls.

--- src/StockportWebapp/Utils/ContentSecurityPolicyBuilder.cs
+++ src/StockportWebapp/Utils/ContentSecurityPolicyBuilder.cs
@@ -52,11 +52,6 @@ namespace StockportWebapp.Utils
 
         private void BuildImageSource()
         {
-            /*khms0.googleapis.com khms1.googleapis.com geo0.ggpht.com geo1.ggpht.com geo2.ggpht.com geo3.ggpht.com  cbks0.googleapis.com csi.gstatic.com " +
-                                 "maps.gstatic.com maps.googleapis.com images.contentful.com/ www.google-analytics.com/r/collect www.google-analytics.com/collect stats.g.doubleclick.net/r/collect " +
-                                 "https://s3-eu-west-1.amazonaws.com/live-iag-static-assets/" +
-                                 "s3-eu-west-1.amazonaws.com/ share.typeform.com/ cdnjs.cloudflare.com/ajax/libs/cookieconsent2/ " +
-                                 "customer.cludo.com/img/ uk1.siteimprove.com/ stockportb.logo-net.co.uk/ *.cloudfront.net/butotv/; " +*/
             _stringBuilder.Append(
                 new ContentSecurityPolicyElement
                     ("img-src")
@@ -87,8 +82,6 @@ namespace StockportWebapp.Utils
 
         private void BuildStyleSource()
         {
-            /*'unsafe-inline' *.cludo.com/css/ s3-eu-west-1.amazonaws.com/ share.typeform.com/ maxcdn.bootstrapcdn.com/font-awesome/ fonts.googleapis.com/ " +
-                                 "cdnjs.cloudflare.com/ajax/libs/cookieconsent2/ *.cloudfront.net/butotv/; " +*/
             _stringBuilder.Append(
                 new ContentSecurityPolicyElement
                     ("style-src")
@@ -105,10 +98,6 @@ namespace StockportWebapp.Utils
 
         private void BuildScriptSource()
         {
-            /*'unsafe-inline' 'unsafe-eval' https://ajax.googleapis.com/ajax/libs/jquery/ maps.googleapis.com m.addthisedge.com/live/boost/ " +
-                                 "www.google-analytics.com/analytics.js api.cludo.com/scripts/ customer.cludo.com/scripts/ cdnjs.cloudflare.com/ajax/libs/cookieconsent2/ " +
-                                 "s3-eu-west-1.amazonaws.com/ share.typeform.com/ js.buto.tv/video/ s7.addthis.com/js/300/addthis_widget.js m.addthis.com/live/ " +
-                                 "siteimproveanalytics.com/js/ *.logo-net.co.uk/Delivery/; " +*/
             _stringBuilder.Append(
                 new ContentSecurityPolicyElement
                     ("script-src")
@@ -132,7 +121,6 @@ namespace StockportWebapp.Utils
 
         private void BuildConnectSource()
         {
-            // https://api.cludo.com/ buto-ping-middleman.buto.tv/ m.addthis.com/live/; " +
             _stringBuilder.Append(
                 new ContentSecurityPolicyElement
                     ("connect-src")
@@ -144,7 +132,6 @@ namespace StockportWebapp.Utils
 
         private void BuildMediaSource()
         {
-            // https://www.youtube.com/ *.cloudfront.net/butotv/live/videos/; " +
             _stringBuilder.Append(
                 new ContentSecurityPolicyElement
                     ("media-src")
@@ -155,7 +142,6 @@ namespace StockportWebapp.Utils
 
         private void BuildObjectSource()
         {
-            // https://www.youtube.com http://www.youtube.com
             _stringBuilder.Append(
                 new ContentSecurityPolicyElement
                     ("object-src")
