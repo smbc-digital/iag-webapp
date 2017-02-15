bccb06d4576a8a68a1e49540c208b5ce9da41b91
SCN\clare.sudbery <clare.sudbery@thoughtworks.gov.uk>
2017-02-15 10:45:24 +0000

    David/Clare #780 Refactor for readability. Also fix errors with urls running into each other.

--- src/StockportWebapp/Utils/ContentSecurityPolicyBuilder.cs
+++ src/StockportWebapp/Utils/ContentSecurityPolicyBuilder.cs
@@ -4,37 +4,62 @@ namespace StockportWebapp.Utils
 {
     public class ContentSecurityPolicyBuilder
     {
-        private string BuildDefaultSource()
+        private StringBuilder _stringBuilder = new StringBuilder();
+
+        public string BuildPolicy()
         {
-            return new ContentSecurityPolicyElement("default-src", containsSelf: false)
+            BuildDefaultSource();
+            BuildChildSource();
+            BuildFontSource();
+            BuildImageSource();
+            BuildStyleSource();
+            BuildScriptSource();
+            BuildConnectSource();
+            BuildMediaSource();
+            BuildObjectSource();
+
+            return _stringBuilder.ToString();
+        }
+
+        private void BuildDefaultSource()
+        {
+            _stringBuilder.Append(
+                new ContentSecurityPolicyElement
+                    ("default-src", containsSelf: false)
                 .AddSource("https:")
-                .Finish();
+                .Finish());
         }
 
-        private string BuildChildSource()
+        private void BuildChildSource()
         {
-            return new ContentSecurityPolicyElement("child-src")
+            _stringBuilder.Append(
+                new ContentSecurityPolicyElement
+                    ("child-src")
                 .AddSource("http://s7.addthis.com")
-                .Finish();
+                .Finish());
         }
 
-        private string BuildFontSource()
+        private void BuildFontSource()
         {
-            return new ContentSecurityPolicyElement("font-src")
+            _stringBuilder.Append(
+                new ContentSecurityPolicyElement
+                    ("font-src")
                 .AddSource("font.googleapis.com")
                 .AddSource("maxcdn.bootstrapcdn.com/font-awesome/")
                 .AddSource("fonts.gstatic.com/")
-                .Finish();
+                .Finish());
         }
 
-        private string BuildImageSource()
+        private void BuildImageSource()
         {
             /*khms0.googleapis.com khms1.googleapis.com geo0.ggpht.com geo1.ggpht.com geo2.ggpht.com geo3.ggpht.com  cbks0.googleapis.com csi.gstatic.com " +
                                  "maps.gstatic.com maps.googleapis.com images.contentful.com/ www.google-analytics.com/r/collect www.google-analytics.com/collect stats.g.doubleclick.net/r/collect " +
                                  "https://s3-eu-west-1.amazonaws.com/live-iag-static-assets/" +
-                                 "s3-eu-west-1.amazonaws.com/share.typeform.com/ cdnjs.cloudflare.com/ajax/libs/cookieconsent2/ " +
+                                 "s3-eu-west-1.amazonaws.com/ share.typeform.com/ cdnjs.cloudflare.com/ajax/libs/cookieconsent2/ " +
                                  "customer.cludo.com/img/ uk1.siteimprove.com/ stockportb.logo-net.co.uk/ *.cloudfront.net/butotv/; " +*/
-            return new ContentSecurityPolicyElement("img-src")
+            _stringBuilder.Append(
+                new ContentSecurityPolicyElement
+                    ("img-src")
                 .AddSource("khms0.googleapis.com")
                 .AddSource("khms1.googleapis.com")
                 .AddSource("geo0.ggpht.com")
@@ -50,37 +75,43 @@ namespace StockportWebapp.Utils
                 .AddSource("www.google-analytics.com/collect")
                 .AddSource("stats.g.doubleclick.net/r/collect")
                 .AddSource("https://s3-eu-west-1.amazonaws.com/live-iag-static-assets/")
-                .AddSource("s3-eu-west-1.amazonaws.com/share.typeform.com/")
+                .AddSource("s3-eu-west-1.amazonaws.com/")
+                .AddSource("share.typeform.com/")
                 .AddSource("cdnjs.cloudflare.com/ajax/libs/cookieconsent2/")
                 .AddSource("customer.cludo.com/img/")
                 .AddSource("uk1.siteimprove.com/")
                 .AddSource("stockportb.logo-net.co.uk/")
                 .AddSource("*.cloudfront.net/butotv/")
-                .Finish();
+                .Finish());
         }
 
-        private string BuildStyleSource()
+        private void BuildStyleSource()
         {
-            /*'unsafe-inline' *.cludo.com/css/ s3-eu-west-1.amazonaws.com/share.typeform.com/ maxcdn.bootstrapcdn.com/font-awesome/ fonts.googleapis.com/ " +
+            /*'unsafe-inline' *.cludo.com/css/ s3-eu-west-1.amazonaws.com/ share.typeform.com/ maxcdn.bootstrapcdn.com/font-awesome/ fonts.googleapis.com/ " +
                                  "cdnjs.cloudflare.com/ajax/libs/cookieconsent2/ *.cloudfront.net/butotv/; " +*/
-            return new ContentSecurityPolicyElement("style-src")
+            _stringBuilder.Append(
+                new ContentSecurityPolicyElement
+                    ("style-src")
                 .AddSource("'unsafe-inline'")
                 .AddSource("*.cludo.com/css/")
-                .AddSource("s3-eu-west-1.amazonaws.com/share.typeform.com/")
+                .AddSource("s3-eu-west-1.amazonaws.com/")
+                .AddSource("share.typeform.com/")
                 .AddSource("maxcdn.bootstrapcdn.com/font-awesome/")
                 .AddSource("fonts.googleapis.com/")
                 .AddSource("cdnjs.cloudflare.com/ajax/libs/cookieconsent2/")
                 .AddSource("*.cloudfront.net/butotv/")
-                .Finish();
+                .Finish());
         }
 
-        private string BuildScriptSource()
+        private void BuildScriptSource()
         {
             /*'unsafe-inline' 'unsafe-eval' https://ajax.googleapis.com/ajax/libs/jquery/ maps.googleapis.com m.addthisedge.com/live/boost/ " +
                                  "www.google-analytics.com/analytics.js api.cludo.com/scripts/ customer.cludo.com/scripts/ cdnjs.cloudflare.com/ajax/libs/cookieconsent2/ " +
-                                 "s3-eu-west-1.amazonaws.com/share.typeform.com/ js.buto.tv/video/ s7.addthis.com/js/300/addthis_widget.js m.addthis.com/live/ " +
+                                 "s3-eu-west-1.amazonaws.com/ share.typeform.com/ js.buto.tv/video/ s7.addthis.com/js/300/addthis_widget.js m.addthis.com/live/ " +
                                  "siteimproveanalytics.com/js/ *.logo-net.co.uk/Delivery/; " +*/
-            return new ContentSecurityPolicyElement("script-src")
+            _stringBuilder.Append(
+                new ContentSecurityPolicyElement
+                    ("script-src")
                 .AddSource("'unsafe-inline'")
                 .AddSource("'unsafe-eval'")
                 .AddSource("https://ajax.googleapis.com/ajax/libs/jquery/")
@@ -89,67 +120,48 @@ namespace StockportWebapp.Utils
                 .AddSource("api.cludo.com/scripts/")
                 .AddSource("customer.cludo.com/scripts/")
                 .AddSource("cdnjs.cloudflare.com/ajax/libs/cookieconsent2/")
-                .AddSource("s3-eu-west-1.amazonaws.com/share.typeform.com/")
+                .AddSource("s3-eu-west-1.amazonaws.com/")
+                .AddSource("share.typeform.com/")
                 .AddSource("js.buto.tv/video/")
                 .AddSource("s7.addthis.com/js/300/addthis_widget.js")
                 .AddSource("m.addthis.com/live/")
                 .AddSource("siteimproveanalytics.com/js/")
                 .AddSource("*.logo-net.co.uk/Delivery/")
-                .Finish();
+                .Finish());
         }
 
-        private string BuildConnectSource()
+        private void BuildConnectSource()
         {
             // https://api.cludo.com/ buto-ping-middleman.buto.tv/ m.addthis.com/live/; " +
-            return new ContentSecurityPolicyElement("connect-src")
+            _stringBuilder.Append(
+                new ContentSecurityPolicyElement
+                    ("connect-src")
                 .AddSource("https://api.cludo.com/")
                 .AddSource("buto-ping-middleman.buto.tv/")
                 .AddSource("m.addthis.com/live/")
-                .Finish();
+                .Finish());
         }
 
-        private string BuildMediaSource()
+        private void BuildMediaSource()
         {
             // https://www.youtube.com/ *.cloudfront.net/butotv/live/videos/; " +
-            return new ContentSecurityPolicyElement("media-src")
+            _stringBuilder.Append(
+                new ContentSecurityPolicyElement
+                    ("media-src")
                 .AddSource("https://www.youtube.com/")
                 .AddSource("*.cloudfront.net/butotv/live/videos/")
-                .Finish();
+                .Finish());
         }
 
-        private string BuildObjectSource()
+        private void BuildObjectSource()
         {
             // https://www.youtube.com http://www.youtube.com
-            return new ContentSecurityPolicyElement("object-src")
+            _stringBuilder.Append(
+                new ContentSecurityPolicyElement
+                    ("object-src")
                 .AddSource("https://www.youtube.com")
                 .AddSource("http://www.youtube.com")
-                .Finish();
-        }
-
-        public string BuildPolicy()
-        {
-            var stringBuilder = new StringBuilder();
-            stringBuilder.Append(BuildDefaultSource());
-            stringBuilder.Append(BuildChildSource());
-            stringBuilder.Append(BuildFontSource());
-            stringBuilder.Append(BuildImageSource());
-            stringBuilder.Append(BuildStyleSource());
-            stringBuilder.Append(BuildScriptSource());
-            stringBuilder.Append(BuildConnectSource());
-            stringBuilder.Append(BuildMediaSource());
-            stringBuilder.Append(BuildObjectSource());
-
-            return stringBuilder.ToString();
-
-            //return BuildDefaultSource() +
-            //       BuildChildSource() +
-            //       BuildFontSource() +
-            //       BuildImageSource() +
-            //       BuildStyleSource() +
-            //       BuildScriptSource() +
-            //       "connect-src 'self' https://api.cludo.com/ buto-ping-middleman.buto.tv/ m.addthis.com/live/; " +
-            //       "media-src 'self' https://www.youtube.com/ *.cloudfront.net/butotv/live/videos/; " +
-            //       "object-src 'self' https://www.youtube.com http://www.youtube.com; "; 
+                .Finish());
         }
     }
 }
