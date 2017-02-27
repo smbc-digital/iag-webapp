0848173115b246fe2978daa3cae611aea810242e
David Meek <david.meek@stockport.gov.uk>
2017-02-27 10:53:59 +0000

    David/SimonE #756 - Adding feature toggle to group link on event page

--- src/StockportWebapp/FeatureToggling/FeatureToggles.cs
+++ src/StockportWebapp/FeatureToggling/FeatureToggles.cs
@@ -7,5 +7,6 @@ namespace StockportWebapp.FeatureToggling
         public bool NewsroomPagination { get; set; } = false;
         public bool EventsPagination { get; set; } = false;
         public bool TopicPageImages { get; set; } = false;
+        public bool GroupLinkOnEvent { get; set; } = false;
     }
 }
