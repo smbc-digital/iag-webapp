e38570db342cd57f69536d81fc31819ec868c659
Gary Holland <gary.holland@stockport.gov.uk>
2017-03-06 10:44:36 +0000

    Gary/Clare #817 wrote failing test

---
+++ src/StockportWebapp/Utils/PaginationHelper.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.AspNetCore.Routing;
+
+namespace StockportWebapp.Utils
+{
+    public interface IPaginationHelper
+    {
+    }
+
+    public class PaginationHelper : IPaginationHelper
+    {
+        public int CalculateIndexOfFirstItemOnPage(int currentPageNumber, int numItemsOnPage)
+        {
+            return 890;
+        }
+    }
+}
---
+++ test/StockportWebappTests/Unit/Utils/PaginationHelperTest.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using FluentAssertions;
+using Microsoft.AspNetCore.Http.Internal;
+using Microsoft.AspNetCore.Routing;
+using Microsoft.Extensions.Primitives;
+using Moq;
+using StockportWebapp.Utils;
+using Xunit;
+
+namespace StockportWebappTests.Unit.Utils
+{
+    public class PaginationHelperTest
+    {
+        [Fact]
+        public void IndexOfFirstItemOnPageOneShouldBeOne()
+        {
+            // Arrange
+            int firstPageItem;
+            const int currentPageNumber = 1;
+            const int numItemsOnPage = 15;
+            var paginationHelper = new PaginationHelper();
+
+            // Act
+            firstPageItem = paginationHelper.CalculateIndexOfFirstItemOnPage(currentPageNumber, numItemsOnPage);
+
+            // Assert
+            firstPageItem.Should().Be(1);
+        }
+    }
+}
