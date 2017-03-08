8351108b77853e5976625af6ed5da91a64a62998
David Meek <david.meek@stockport.gov.uk>
2017-03-08 15:09:44 +0000

    David/SimonE #806 Add search facitility to healthy stockport again

--- src/StockportWebapp/wwwroot/assets/javascript/healthystockport/healthystockport.js
+++ src/StockportWebapp/wwwroot/assets/javascript/healthystockport/healthystockport.js
@@ -1,9 +1,17 @@
 ﻿// Push content on presence of background image
-
+var mobileWidth = 767;
+var tabletWidth = (1024 - 17);
 var windowHeight = $(window).height();
 var pushHeight = (windowHeight / 2) + "px";
 
 $(document).ready(function () {
+    $(".show-search-button").click(
+            function () {
+                $("#mobileSearchInput").slideToggle(220);
+                $(".show-search-button").toggleClass("arrow");
+            }
+    );
+
     if (($('.background-image').css("background-image") !== "none"
         &&
         $('.background-image').css("background-image") !== undefined)
@@ -20,6 +28,8 @@ var target = $('.background-image');
 var targetHeight = $(window).height();
 
 $(document).scroll(function (e) {
+    
+
     if (!$('body').hasClass('type-home')) {
         var y_scroll_pos = $(this).scrollTop();
         var scrollPercent = (targetHeight - (y_scroll_pos * 2.0)) / targetHeight;
@@ -29,5 +39,13 @@ $(document).scroll(function (e) {
         if (scrollPercent <= 0.02) {
             target.css('opacity', 0);
         }
+    }
+});
+
+$(window).resize(function () {
+    if ($(window).width() > tabletWidth) {
+        $("#mobileSearchInput").hide();
+        $(".show-search-button").removeClass("arrow");
+        $('#displayRefineBy').css('display', 'block');
     }
 });
