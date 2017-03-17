c039b8fd59ff990cb752b309ce98e8c53a34f3e7
Neil Redfern <neil.redfern@stockport.gov.uk>
2017-03-17 12:05:20 +0000

    Neil - removed some feature toggles

--- src/StockportWebapp/FeatureToggling/FeatureToggles.cs
+++ src/StockportWebapp/FeatureToggling/FeatureToggles.cs
@@ -2,8 +2,6 @@ namespace StockportWebapp.FeatureToggling
 {
     public class FeatureToggles
     {      
-        public bool FooterCache { get; set; } = false;
-        public bool NewsCustomDateSearch { get; set; } = false;
         public bool NewsroomPagination { get; set; } = false;
         public bool EventsPagination { get; set; } = false;
         public bool TopicPageImages { get; set; } = false;
