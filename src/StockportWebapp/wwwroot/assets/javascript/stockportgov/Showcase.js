d23014fd7b2e9e940e6f7a857cde32c2d17f73d9
Gary Holland <gary.holland@stockport.gov.uk>
2017-03-31 13:48:56 +0100

    Gary/Neil #829 added graphic menu to showcases

---
+++ src/StockportWebapp/wwwroot/assets/javascript/stockportgov/Showcase.js
@@ -0,0 +1,15 @@
+﻿var matchboxFeaturedItemsShowcase = new Matchbox({
+    parentSelector: ".featured-items-wrapper",
+    childSelector: '.featured-topic',
+    groupsOf: 3,
+    breakpoints: [
+    { bp: 767, groupsOf: 4 },
+    { bp: 1024, groupsOf: 5 }
+    ]
+});
+
+$(document).ready(
+    function () {
+        if ($(".featured-items-wrapper").length) { matchboxFeaturedItemsShowcase.init(); }
+    }()
+);
