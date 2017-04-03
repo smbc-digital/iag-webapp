6b69a4ab8c6957763d274b3a3e98b30f8e970315
davidmeekstockport <david.meek@stockport.gov.uk>
2017-04-03 15:02:04 +0100

    David/Raj #849 - Creating start page for groups

---
+++ src/StockportWebapp/Models/GroupCategory.cs
@@ -0,0 +1,17 @@
+﻿using StockportWebapp.Validation;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using StockportWebapp.Utils;
+
+namespace StockportWebapp.Models
+{
+    public class GroupCategory
+    {
+        public string Name { get; set; }      
+        public string Icon { get; set; }    
+
+        public GroupCategory() { }
+        
+    }
+}
