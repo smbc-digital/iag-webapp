f8b733e27ea3ba59a0f560f9aade08f0ecefc77c
Gary Holland <gary.holland@stockport.gov.uk>
2017-04-13 12:47:13 +0100

    gary/raj #851 automatically resize selects

---
+++ src/StockportWebapp/wwwroot/assets/javascript/stockportgov/PrimaryFilter.js
@@ -0,0 +1,24 @@
+﻿$(document).ready(function () {
+
+    $("#hiddenSelectCategory").html("<option>" + $("#selectCategory").find(":selected").text() + "</option>");
+    $("#selectCategory").width($("#hiddenSelectCategory").width());
+    $("#selectCategory").change(function () {
+        $("#hiddenSelectCategory").html("<option>" + $("#selectCategory").find(":selected").text() + "</option>");
+        $("#selectCategory").width($("#hiddenSelectCategory").width());
+    });
+
+    $("#hiddenSelectOrder").html("<option>" + $("#selectOrder").find(":selected").text() + "</option>");
+    $("#selectOrder").width($("#hiddenSelectOrder").width());
+    $("#selectOrder").change(function () {
+        $("#hiddenSelectOrder").html("<option>" + $("#selectOrder").find(":selected").text() + "</option>");
+        $("#selectOrder").width($("#hiddenSelectOrder").width());
+    });
+
+    $("#hiddenSelectLocation").html("<option>" + $("#selectLocation").find(":selected").text() + "</option>");
+    $("#selectLocation").width($("#hiddenSelectLocation").width());
+    $("#selectLocation").change(function () {
+        $("#hiddenSelectLocation").html("<option>" + $("#selectOrder").find(":selected").text() + "</option>");
+        $("#selectLocation").width($("#hiddenSelectLocation").width());
+    });
+
+});
