45bf5d3a44d73b588cdb183e0370c6e03b0c7441
Matthew Lloyd <matthew.lloyd@stockport.gov.uk>
2017-04-26 15:23:44 +0100

    Matt / Alina / Not Jedops because he's ignored us - Added the updated buildscript

--- build.ps1
+++ build.ps1
@@ -1,36 +1,60 @@
-function _Build($appName, $projectPath) {
-  _DeleteFile "$appName.zip"
-  cd $projectPath
-  dotnet restore
-  cd ..\..
-}
+param (
+  [string]$appVersion = "local-build"
+)
+
+$appName = "iag-webapp"
+$projectPath = "src/StockportWebapp"
+$testDir = "test/StockportWebappTests"
+$publishDir = "$projectPath/publish"
 
 function _DeleteFile($fileName) {
-    If (Test-Path $fileName) {
-        Write-Host "Deleting '$fileName'"
-  	   Remove-Item $fileName
-    } else {
-      "'$fileName' not found. Nothing deleted"
-    }
+  If (Test-Path $fileName) {
+    Write-Host "Deleting '$fileName'"
+    Remove-Item $fileName
+  } else {
+    "'$fileName' not found. Nothing deleted"
+  }
 }
 
-function _Publish($projectPath) {
-  cd $projectPath
+function _Clean() {
+  echo "Cleaning content"
+  _DeleteFile "$appName.zip"
+}
+
+function _DotnetRestore() {
+  echo "Running dotnet restore"
+  dotnet restore
+}
+
+function _DotnetTest() {
+  echo "Running dotnet test"
+  pushd $testDir
+  dotnet test
+  popd
+}
+
+function _Version() {
+  echo "Versioning App"
+  # TODO: Removed this line as git isn't available in powershell right now.
+  # git rev-parse HEAD | Out-File "src/$projectName/sha.txt"
+  echo $appVersion | Out-File "$projectPath/version.txt"
+}
+
+function _Publish() {
+  echo "Publishing App"
+  pushd $projectPath
   dotnet publish --configuration Release -o publish
-  cd ..\..
-}
-
-function _Package($name) {
-  python zip.py src\StockportContentApi\publish "$name.zip"
+  popd
 }
 
 function Main {
   Try {
-    $appName = "webapp"
-    $projectPath = "src\StockportWebapp"
-
-    _Build $appName $projectPath
-    _Publish $projectPath
+    # clean dotnet-restore dotnet-test version publish-app package-app
+    _Clean
+    _DotnetRestore
+    _DotnetTest
+    _Version
+    _Publish
   }
   Catch {
     Write-Error $_.Exception
