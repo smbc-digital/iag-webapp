4fc1de91cd8a549b40989cd6c3da41192ac7be99
Gary Holland <gary.holland@stockport.gov.uk>
2017-05-19 11:52:13 +0100

    David/Gary #912 - Adding header to email event template

--- src/StockportWebapp/Repositories/EventsRepository.cs
+++ src/StockportWebapp/Repositories/EventsRepository.cs
@@ -38,16 +38,21 @@ namespace StockportWebapp.Repositories
         public string GenerateEmailBody(EventSubmission eventSubmission)
         {
             var stringBuilder = new StringBuilder();
+            stringBuilder.Append("<table border=\"0\" cellpadding=\"0\" cellspacing=\"0\" id=\"templateHeader\" style=\"background-color: #F4F4F4; border-collapse: collapse !important; mso-table-lspace: 0pt; mso-table-rspace: 0pt;\" width=\"100%\">");
+            stringBuilder.Append("<tr>");
+            stringBuilder.Append("<td class=\"headerContent\" style=\"background-color: #055C58; font-family: Helvetica; font-size: 20px; font-weight: bold; line-height: 100%; mso-table-lspace: 0pt; mso-table-rspace: 0pt; padding-bottom: 10px; padding-left: 10px; padding-right: 0; padding-top: 10px; text-align: left; vertical-align: middle;\" valign=\"top\"><img class=\"flexibleImage\" src=\"https://s3-eu-west-1.amazonaws.com/live-iag-static-assets/logo-stockport-full%402x.png\" style =\"border: 0; height: auto; line-height: 100%; max-width: 225px; outline: none; text-decoration: none; width: 225px;\" width=\"225\" /></td>");
+            stringBuilder.Append("</tr>");
+            stringBuilder.Append("</table>");
 
-            stringBuilder.Append("<p>Thank you for submitting an event</p>");
-            stringBuilder.Append("<p>Before your event is added to the calendar, it will be reviewed and if it is successful then it will be published on the website.</p>");
-            stringBuilder.Append("<h1>Event submission</h1>");
+            stringBuilder.Append("<h2 style=\"font-family: 'Source Sans Pro', sans-serif\">Thank you for adding an event</h2>");
+            stringBuilder.Append("<p style=\"font-family: 'Source Sans Pro', sans-serif\">Before your event is added to the calendar, it will be reviewed and if it is successful then it will be published on the website.</p>");
+            stringBuilder.Append("<h2 style=\"font-family: 'Source Sans Pro', sans-serif\">Your event</h2>");
 
-            stringBuilder.Append($"<p>Event name: {eventSubmission.Title}<br />");
+            stringBuilder.Append($"<p style=\"font-family: 'Source Sans Pro', sans-serif\">Event name: {eventSubmission.Title}<br />");
             if (eventSubmission.EventDate.HasValue) stringBuilder.Append($"Event date: {eventSubmission.EventDate.Value:dddd dd MMMM yyyy}<br />");
             if (eventSubmission.StartTime.HasValue) stringBuilder.Append($"Start time: {eventSubmission.StartTime.Value:HH:mm}<br />");
             if (eventSubmission.EndTime.HasValue) stringBuilder.Append($"End time: {eventSubmission.EndTime.Value:HH:mm}<br />");
-            stringBuilder.Append($"How often does your event occur?: {eventSubmission.Frequency}<br />");
+            stringBuilder.Append($"Frequency: {eventSubmission.Frequency}<br />");
             if (eventSubmission.EndDate.HasValue) stringBuilder.Append($"End date: {eventSubmission.EndDate.Value:dddd dd MMMM yyyy}<br />");
             stringBuilder.Append($"Price: {eventSubmission.Fee}<br />");
             stringBuilder.Append($"Location: {eventSubmission.Location}<br />");            
@@ -63,8 +68,8 @@ namespace StockportWebapp.Repositories
             if (eventSubmission.Attachment != null) stringBuilder.Append($"Additional event document: {FileHelper.GetFileNameFromPath(eventSubmission.Attachment)}<br />");
             stringBuilder.Append($"<br />Organiser email address: {eventSubmission.SubmitterEmail}");
             stringBuilder.Append("</p>");
-            stringBuilder.Append("<p>You can let us know about a change to this event by emailing us at:</p>");
-            stringBuilder.Append(@"<p><a href = ""mailto:&gt;website.updates@stockport.gov.uk""> website.updates@stockport.gov.uk</a></p>");
+            stringBuilder.Append("<p style=\"font-family: 'Source Sans Pro', sans-serif\">You can let us know about a change to this event by emailing us at:</p>");
+            stringBuilder.Append("<p style=\"font-family: 'Source Sans Pro', sans-serif\"><a href = \"mailto:&gt;website.updates@stockport.gov.uk\"> website.updates@stockport.gov.uk</a></p>");
             return stringBuilder.ToString();
         }
 
