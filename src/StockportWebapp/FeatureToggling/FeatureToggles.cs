2d8c4a2eb40fc1cda6490a1fbf153e9f2a7797e5
Nick Lewis <nick.lewis@stockport.gov.uk>
2017-05-23 14:39:45 +0100

    #942 Nick/Gary Removed feature toggles from code

--- src/StockportWebapp/FeatureToggling/FeatureToggles.cs
+++ src/StockportWebapp/FeatureToggling/FeatureToggles.cs
@@ -2,10 +2,6 @@ namespace StockportWebapp.FeatureToggling
 {
     public class FeatureToggles
     {      
-        public bool TopicPageImages { get; set; } = false;
-        public bool SinglePayment { get; set; } = false;
-        public bool HSFooter { get; set; } = false;
-        public bool GroupButtonOnHomePage { get; set; } = false;
         public bool GroupsAutoCompleteLocation { get; set; } = false;
     }
 
