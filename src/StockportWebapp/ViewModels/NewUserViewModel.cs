351cb6eb5270b181dbefc12b047e844e3893ec6a
Nick Lewis <nick.lewis@stockport.gov.uk>
2017-06-13 10:59:35 +0100

    #982 David / Nick Adding new Add User page to groups section

---
+++ src/StockportWebapp/ViewModels/NewUserViewModel.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc.Rendering;
+using StockportWebapp.Models;
+using System.Collections.Generic;
+
+namespace StockportWebapp.ViewModels
+{
+    public class NewUserViewModel
+    {
+        public string Slug { get; set; }
+        public string Name { get; set; }
+        public GroupAdministratorItems GroupAdministratorItem { get; set; }
+        public List<SelectListItem> SelectList { get; set; }
+
+        public NewUserViewModel()
+        {
+            SelectList = new List<SelectListItem>();
+            SelectList.Add(new SelectListItem() { Text = "Administrator", Value = "A" });
+            SelectList.Add(new SelectListItem() { Text = "Editor", Value = "E" });
+        }
+    }
+}
