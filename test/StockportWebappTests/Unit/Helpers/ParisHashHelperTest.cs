782aa363c6bb4833116125ae8ab1659a860b4613
davidmeekstockport <david.meek@stockport.gov.uk>
2017-06-20 15:37:30 +0100

    David #964 - Fixing the unit tests now we have a Paris Keys object

--- test/StockportWebappTests/Unit/Helpers/ParisHashHelperTest.cs
+++ test/StockportWebappTests/Unit/Helpers/ParisHashHelperTest.cs
@@ -3,13 +3,14 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using FluentAssertions;
 using Xunit;
 
 namespace StockportWebappTests.Unit.Helpers
 {
     public class ParisHashHelperTest
     {
-        public ParisHashHelper HashHelper { get; set; } = new ParisHashHelper();
+        public ParisHashHelper HashHelper { get; set; } = new ParisHashHelper(new ParisKeys() { PreSalt= "preSalt", PostSalt = "postSalt", PrivateSalt = "privateSalt"});
 
         [Fact]
         public void ShouldReturnFalseIfHashDoesntMatch()
@@ -23,7 +24,7 @@ namespace StockportWebappTests.Unit.Helpers
             var result = HashHelper.IsMatchingHash(url, hash);
 
             // Assert
-            Assert.Equal(result, expectedResult);
+            result.Should().Be(expectedResult);
         }
 
         [Fact]
@@ -32,13 +33,13 @@ namespace StockportWebappTests.Unit.Helpers
             // Arrange
             var expectedResult = true;
             var url = "?authorisationcode=900000&receiptnumber=IP/280885&transactiontype=09&merchantnumber=84800683&data=BikeabilityContribution&serviceprocessed=true&merchanttid=03151707&amount=100.00&date=16/06/2017 14:07:23&administrationcharge=0";
-            var hash = "FC-4A-FB-68-C7-E4-50-76-D3-07-42-71-09-79-48-EE-CB-C3-31-D0-40-CE-82-A6-98-B1-D1-63-4E-22-69-3D-E7-8F-0D-7C-19-98-9C-D5-4E-55-CC-5C-65-83-50-57-4E-45-8C-84-94-7A-4D-ED-E5-7E-A9-60-82-5E-6C-0F";
+            var hash = "96-D7-2B-28-EA-F5-98-AD-D9-99-35-2C-6F-7E-B9-E8-95-EF-28-B2-7F-9D-2C-D2-B5-60-F8-39-FE-51-6B-CD-E8-52-EB-4B-3E-83-48-AA-B8-99-69-06-62-33-26-69-0D-3C-D5-84-FB-76-7A-C8-CA-6E-60-4A-B5-6A-19-EB";
 
             // Act
             var result = HashHelper.IsMatchingHash(url, hash);
 
             // Assert
-            Assert.Equal(result, expectedResult);
+            result.Should().Be(expectedResult);
         }
 
     }
