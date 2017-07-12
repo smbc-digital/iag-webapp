98b15535618bf4e858a498d71a54d294d6672377
Raj Paili <raj.paili@stockport.gov.uk>
2017-07-12 16:14:46 +0100

    fixed merge conflict

--- src/StockportWebapp/FeatureToggling/FeatureToggles.cs
+++ src/StockportWebapp/FeatureToggling/FeatureToggles.cs
@@ -6,12 +6,9 @@ namespace StockportWebapp.FeatureToggling
         public bool ManageYourGroupButton { get; set; } = false;
         public bool ContactUsIds { get; set; } = false;
         public bool ExportGroupToPdf { get; set; } = false;
-<<<<<<< cd99e8543a5517c6df5d87173a01f2696744d371
         public bool NewHomepageLayout { get; set; } = false;
         public bool EditGroupWYSIWYG { get; set; } = false;
         public bool PrimaryItems { get; set; }
-=======
         public bool ExportGroupEventsToCalendar { get; set; } = false;
->>>>>>> #1028 Raj Export event to calendar
     }
 }
