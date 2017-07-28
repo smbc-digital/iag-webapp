aa0e5e83562a490a5811fe607eb89ac427e87c8f
Nick Lewis <nicholaslewis@hotmail.com>
2017-07-28 11:28:01 +0100

    #1094 Nick / Mohammed - New Events Filter Bar

--- src/StockportWebapp/wwwroot/assets/javascript/stockportgov.js
+++ src/StockportWebapp/wwwroot/assets/javascript/stockportgov.js
@@ -447,11 +447,13 @@ STK.RefineBy = (function () {
     var setBadges = function () {
         $('.badge', '#event-listing-refine-bar').css('visibility', 'hidden');
 
+        var allcount = 0;
         $('.refine', '#event-listing-refine-bar').each(function () {
             var count = 0;
             $('input[type=checkbox]', $(this)).each(function () {
                 if ($(this).prop('checked')) {
                     count++;
+                    allcount++;
                 }
             });
 
@@ -459,6 +461,10 @@ STK.RefineBy = (function () {
                 $('.badge', $(this)).html('<span>' + count + '</span>').css('visibility', 'visible');
             }
         });
+
+        if (allcount > 0) {
+            $('.refine-all .badge', '#event-listing-refine-bar').html('<span>' + allcount + '</span>').css('visibility', 'visible');
+        }
     };
 
     var applyFilter = function () {
@@ -487,6 +493,43 @@ STK.RefineBy = (function () {
         return $('input[name=tag]', '#event-listing-refine-bar').val();
     };
 
+    var initialiseSlider = function () {
+        var width = $(window).width();
+        $('#refine-slider').css('left', width);
+    };
+
+    var revealSlider = function () {
+        var top = $(document).scrollTop();
+        $('#refine-slider').css('top', top).removeClass('hide-on-mobile').animate({ 'left': 0 }, 250);
+        var height = $('#refine-slider').height() - $('.update-cancel-bar', '#event-listing-refine-bar').height();
+        $('.scroller', '#refine-slider').height(height);
+        $('body').css('overflow', 'hidden');
+
+        currentState = [];
+        $('input[type=checkbox]', '#event-listing-refine-bar').each(function () {
+            currentState.push($(this).prop('checked'));
+        });
+    };
+
+    var clearHeight = function () {
+        $('.scroller', '#refine-slider').height('');
+    };
+
+    var hideSlider = function () {
+        var width = $(window).width();
+        $('#refine-slider').animate({ 'left': width }, 250, 'swing', function () { $('#refine-slider').addClass('hide-on-mobile'); });
+        $('body').css('overflow', 'scroll');
+        $('input[type=checkbox]', '#event-listing-refine-bar').each(function (index) {
+            $(this).prop('checked', currentState[index]);
+        });
+    };
+
+    var clearAllFilters = function () {
+        $('input[type=checkbox]', '#event-listing-refine-bar').each(function () {
+            $(this).prop('checked', false);
+        });
+    };
+
     return {
         Init: function () {
             $('#event-listing-refine-bar').show();
@@ -508,6 +551,29 @@ STK.RefineBy = (function () {
             $('input[type=checkbox]', '#event-listing-refine-bar').click(function () {
                 setBadges();
             });
+
+            $('#reveal-refine-by').click(function () {
+                revealSlider();
+            });
+
+            $('.update-cancel-bar .cancel', '#event-listing-refine-bar').click(function () {
+                hideSlider();
+            });
+
+            $('.update-cancel-bar .apply', '#event-listing-refine-bar').click(function () {
+                applyFilter();
+            });
+
+            $('.clear-all-filters a', '#event-listing-refine-bar').click(function () {
+                clearAllFilters();
+            });
+
+            $(window).on('resize', function () {
+                clearHeight();
+                hideSlider();
+            });
+
+            initialiseSlider();
         }
     };
 })();
