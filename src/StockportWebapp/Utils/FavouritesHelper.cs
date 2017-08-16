e5e4f955f6488b5b022cefc92cb9ca0caaba3a69
Nick Lewis <nicholaslewis@hotmail.com>
2017-08-16 11:51:25 +0100

    Nick - Adding util skeleton so others can make calls

---
+++ src/StockportWebapp/Utils/FavouritesHelper.cs
@@ -0,0 +1,26 @@
+﻿using StockportWebapp.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace StockportWebapp.Utils
+{
+    public class FavouritesHelper
+    {
+        public List<Group> PopulateFavouriteOnGroups(List<Group> groups)
+        {
+            return groups;
+        }
+
+        public void AddGroupToFavourites(string slug)
+        {
+
+        }
+
+        public void RemoveGroupFromFavourites(string slug)
+        {
+
+        }
+    }
+}
