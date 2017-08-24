8d9ea39378c3b3acc47f324b513d7227d1591ade
davidmeekstockport <david.meek@stockport.gov.uk>
2017-08-24 15:37:09 +0100

    David/Neil #1030 - added default text to the group's volunteering page

--- src/StockportWebapp/Models/GroupSubmission.cs
+++ src/StockportWebapp/Models/GroupSubmission.cs
@@ -58,6 +58,6 @@ namespace StockportWebapp.Models
         public double Longitude { get; set; }
 
         public string VolunteeringText { get; set; } =
-            "If you would like to become a volunteer for this group, please email us with your interest and we'll get in touch if there are any opportunities.";
+            "If you would like to find out more about being a volunteer with us, please e-mail with your interest and we’ll be in contact as soon as possible.";
     }
 }
