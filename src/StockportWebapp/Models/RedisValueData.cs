39ffb7a5298ec080b55cabd15f90ab11bcdf1101
davidmeekstockport <david.meek@stockport.gov.uk>
2017-09-01 16:00:56 +0100

    Stephen/David #1203 - Various Redis Upgrades

---
+++ src/StockportWebapp/Models/RedisValueData.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace StockportWebapp.Models
+{
+    public class RedisValueData
+    {
+        public string Key { get; set; }
+        public string Expiry { get; set; }
+        public int NumberOfItems { get; set; }
+    }
+}
