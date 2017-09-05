02d23e858a263ba9a239f5d1c45cdc3231c06773
Raj Paili <raj.paili@stockport.gov.uk>
2017-09-05 11:31:09 +0100

    #1140 Test-fixed on iphone horizontal view

--- src/StockportWebapp/wwwroot/assets/javascript/stockportgov/RefineByBar.js
+++ src/StockportWebapp/wwwroot/assets/javascript/stockportgov/RefineByBar.js
@@ -148,7 +148,7 @@
 
     var revealSlider = function () {
         var top = $(document).scrollTop();
-        $('#refine-slider').css('top', top).removeClass('hide-on-mobile').animate({ 'left': 0 }, 250);
+        $('#refine-slider').css('top', '0').removeClass('hide-on-mobile').animate({ 'left': 0 }, 250);
         windowWidth = $(window).width();
         var height = $(window).height() - 50 - $('.update-cancel-bar', '#event-listing-refine-bar').height();
         if (windowWidth > height) {
