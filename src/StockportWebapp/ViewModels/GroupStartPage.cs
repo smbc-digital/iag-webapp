ecf4cdc114e423cf59d74700fdf53b2d7278c641
Nick Lewis <nicholaslewis@hotmail.com>
2017-09-05 10:00:59 +0100

    #1185 - Nick - Adding view more categories to groups

--- src/StockportWebapp/ViewModels/GroupStartPage.cs
+++ src/StockportWebapp/ViewModels/GroupStartPage.cs
@@ -9,6 +9,22 @@ namespace StockportWebapp.ViewModels
         public PrimaryFilter PrimaryFilter { set; get; }
 
         public GroupStartPage() { }
-        
+
+        public GenericFeaturedItemList GenericItemList
+        {
+            get
+            {
+                var result = new GenericFeaturedItemList();
+                result.Items = new List<GenericFeaturedItem>();
+                foreach (var cat in Categories)
+                {
+                    result.Items.Add(new GenericFeaturedItem { Icon = cat.Icon, Title = cat.Name, Url = $"/events?category={cat.Slug}" });
+                }
+
+                result.ButtonText = "View more categories";
+
+                return result;
+            }
+        }
     }
 }
