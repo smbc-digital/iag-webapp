d559caa7d06c73cad832c5b80a0789e50eac8c6c
Harsha Reddy <harsha.reddy@stockport.gov.uk>
2017-10-02 11:51:21 +0100

    harsha - added UI-test for democracy page.

---
+++ test/StockportWebappTests/UI/page_objects/stockportgov/democracy.js
@@ -0,0 +1,30 @@
+var methods = {
+    assertTitleIsVisible: function(browser) {
+      browser.assert.title("{pagetitle} - Stockport Council");
+    },
+
+    assertBreadcrumbVisible: function(browser) {
+      browser.useCss();
+        this.waitForElementVisible('.breadcrumb', this.api.globals.timeOut)
+        .expect.element('.breadcrumb').text.to.contain("Home › About your council › Councillors › {breadcrumb}");
+
+    },
+
+    asserSideMenuVisible: function (browser) {
+      browser.useCss();
+      this.waitForElementVisible('.l-left-side-bar-section', this.api.globals.timeOut)
+      .expect.element('.l-left-side-bar-section').text.to.contain("Constitution");
+    }
+};
+
+module.exports = {
+    commands: [methods],
+    url: function() {
+        // This is giving us a page object for the index method
+        return this.api.globals.testUri + '/externaltemplates/democracy';
+    },
+    elements: {
+        articleTitle: 'h1',
+
+  }
+};
