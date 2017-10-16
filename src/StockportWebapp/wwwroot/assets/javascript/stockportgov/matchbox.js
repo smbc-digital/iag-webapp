a7c54304decf4dd32d99475073b4768e50abbd28
Neil Redfern <neil.redfern@stockport.gov.uk>
2017-10-16 16:29:34 +0100

    Neil - changed matchheight to find all child elements under parent

--- src/StockportWebapp/wwwroot/assets/javascript/stockportgov/matchbox.js
+++ src/StockportWebapp/wwwroot/assets/javascript/stockportgov/matchbox.js
@@ -1,23 +1,9 @@
 ﻿define(["jquery"], function ($) {
-
-    var self = this;
-    var matchboxes = [];
-
-    var populate = function () {
-        $('.matchbox-parent').each(function() {
-            $(this).children('.matchbox-child').matchHeight({
-                byRow: true
-            });
-
-            $(this).children().children('.matchbox-child').matchHeight({
-                byRow: true
-            });
-        });
-    };
-
     return {
         Init: function () {
-            populate();
+            $('.matchbox-parent').each(function() { 
+                $('.matchbox-child', this).matchHeight({ byRow: true }); 
+            });
         }
     };
 });
