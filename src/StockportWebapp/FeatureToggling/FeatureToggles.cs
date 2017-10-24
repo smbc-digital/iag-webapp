d0e081f3d83a0c394b4a23fadc3fcd1ffab35027
davidmeekstockport <david.meek@stockport.gov.uk>
2017-10-24 13:45:37 +0100

    David/JonC #1324 - Removed feature toggles

--- src/StockportWebapp/FeatureToggling/FeatureToggles.cs
+++ src/StockportWebapp/FeatureToggling/FeatureToggles.cs
@@ -2,7 +2,6 @@ namespace StockportWebapp.FeatureToggling
 {
     public class FeatureToggles
     {                    
-        public bool AccessibleTransport { get; set; } = false;
-        public bool GroupAdditionalInformation { get; set; } = false;
+       public bool GroupAdditionalInformation { get; set; } = false;
     }
 }
