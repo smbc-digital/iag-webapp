60626793d04f42f327f72ec7f0806cd6f844d429
Neil Redfern <neil.redfern@stockport.gov.uk>
2017-11-06 14:28:46 +0000

    Neil/Jon #44 - added extra repo tests

--- test/StockportWebappTests/Unit/Repositories/StockportApiRepositoryTests.cs
+++ test/StockportWebappTests/Unit/Repositories/StockportApiRepositoryTests.cs
@@ -15,22 +15,31 @@ namespace StockportWebappTests.Unit.Repositories
 {
     public class StockportApiRepositoryTests
     {
+        private Mock<IHttpClient> _httpClient;
+        private Mock<IApplicationConfiguration> _applicationConfiguraiton;
+        private Mock<IUrlGeneratorSimple> _simpleUrlGenerator;
+        private Mock<ILogger<BaseRepository>> _logger;
+
+        public StockportApiRepositoryTests()
+        {
+            _httpClient = new Mock<IHttpClient>();
+            _applicationConfiguraiton = new Mock<IApplicationConfiguration>();
+            _simpleUrlGenerator = new Mock<IUrlGeneratorSimple>();
+            _logger = new Mock<ILogger<BaseRepository>>();
+        }
+
         [Fact]
         public async void GetResponse_ShouldReturnEvent()
         {
             // Arrange
-            var httpClient = new Mock<IHttpClient>();
-            var applicationConfiguraiton = new Mock<IApplicationConfiguration>();
-            var simpleUrlGenerator = new Mock<IUrlGeneratorSimple>();
-            var logger = new Mock<ILogger<BaseRepository>>();
-            var stockportApiRepository = new StockportApiRepository(httpClient.Object, applicationConfiguraiton.Object, simpleUrlGenerator.Object, logger.Object);
+            var stockportApiRepository = new StockportApiRepository(_httpClient.Object, _applicationConfiguraiton.Object, _simpleUrlGenerator.Object, _logger.Object);
             var builtEvents = new List<Event>();
             builtEvents.Add(new EventBuilder().Build());
             var seralisedEvents = JsonConvert.SerializeObject(builtEvents);
 
             // Mock
-            simpleUrlGenerator.Setup(o => o.StockportApiUrl<List<Event>>()).Returns("url");
-            httpClient.Setup(o => o.Get(It.IsAny<string>(), It.IsAny<Dictionary<string, string>>()))
+            _simpleUrlGenerator.Setup(o => o.StockportApiUrl<List<Event>>()).Returns("url");
+            _httpClient.Setup(o => o.Get(It.IsAny<string>(), It.IsAny<Dictionary<string, string>>()))
                 .ReturnsAsync(new HttpResponse(200, seralisedEvents, string.Empty));
 
             // Act
@@ -42,26 +51,44 @@ namespace StockportWebappTests.Unit.Repositories
         }
 
         [Fact]
-        public async void GetResponse_LogIfExceptionIsThrown()
+        public async void GetResponseWithSlugAndQueries_ShouldReturnEvent()
         {
-            //Arrange
-            var httpClient = new Mock<IHttpClient>();
-            var applicationConfiguraiton = new Mock<IApplicationConfiguration>();
-            var simpleUrlGenerator = new Mock<IUrlGeneratorSimple>();
-            var logger = new Mock<ILogger<BaseRepository>>();
-            var stockportApiRepository = new StockportApiRepository(httpClient.Object, applicationConfiguraiton.Object, simpleUrlGenerator.Object, logger.Object);
+            // Arrange
+            var stockportApiRepository = new StockportApiRepository(_httpClient.Object, _applicationConfiguraiton.Object, _simpleUrlGenerator.Object, _logger.Object);
             var builtEvents = new List<Event>();
             builtEvents.Add(new EventBuilder().Build());
             var seralisedEvents = JsonConvert.SerializeObject(builtEvents);
 
             // Mock
-            simpleUrlGenerator.Setup(o => o.StockportApiUrl<List<Event>>()).Returns("url");
-            httpClient.Setup(o => o.Get(It.IsAny<string>(), It.IsAny<Dictionary<string, string>>())).ThrowsAsync(new System.Exception());
+            _simpleUrlGenerator.Setup(o => o.StockportApiUrl<List<Event>>()).Returns("url");
+            _httpClient.Setup(o => o.Get(It.IsAny<string>(), It.IsAny<Dictionary<string, string>>()))
+                .ReturnsAsync(new HttpResponse(200, seralisedEvents, string.Empty));
+
+            // Act
+            var apiResponse = await stockportApiRepository.GetResponse<List<Event>>("slug", new List<Query>() { new Query("name","value")});
+
+            // Assert
+            apiResponse.Should().NotBeNull();
+            apiResponse.ShouldBeEquivalentTo(builtEvents);
+        }
+
+        [Fact]
+        public async void GetResponse_LogIfExceptionIsThrown()
+        {
+            //Arrange
+            var stockportApiRepository = new StockportApiRepository(_httpClient.Object, _applicationConfiguraiton.Object, _simpleUrlGenerator.Object, _logger.Object);
+            var builtEvents = new List<Event>();
+            builtEvents.Add(new EventBuilder().Build());
+            var seralisedEvents = JsonConvert.SerializeObject(builtEvents);
+
+            // Mock
+            _simpleUrlGenerator.Setup(o => o.StockportApiUrl<List<Event>>()).Returns("url");
+            _httpClient.Setup(o => o.Get(It.IsAny<string>(), It.IsAny<Dictionary<string, string>>())).ThrowsAsync(new System.Exception());
 
             // Act
             var apiResponse = await stockportApiRepository.GetResponse<List<Event>>();
 
-            LogTesting.Assert(logger, LogLevel.Error, $"Error getting response for url url");
+            LogTesting.Assert(_logger, LogLevel.Error, "Error getting response for url url");
             apiResponse.Should().BeNull();
         }
     }
