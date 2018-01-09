41943456f31c77ead85f812b215a8b81efd1fdcd
Zico Harris <zico.harris@stockport.gov.uk>
2018-01-09 16:04:45 +0000

    Zico - UItest updates

--- test/StockportWebappTests/UI/specs/healthystockport/userjourney.tests.js
+++ test/StockportWebappTests/UI/specs/healthystockport/userjourney.tests.js
@@ -2,22 +2,22 @@ module.exports = {
     before: function (browser) {
         var homepage = browser.page.healthystockport.homepage();
         homepage.navigate();
-        browser.useCss().assert.visible("div.topic-block-content h4");
+		browser.maximizeWindow();
     },
 
-    'Navigating to article "UITEST: A balanced diet", navigating to the contact us page and then returning to the homepage': function (browser) {
-        var homepage = browser.page.healthystockport.homepage();
-        homepage.goToTopicListBlockPage(browser, "UITEST: A balanced diet", "/topic/uitest-healthy-weight");
+  //  'Navigating to article UITEST: A balanced diet, navigating to the contact us page and then returning to the homepage': function (browser) {
+  //      var homepage = browser.page.healthystockport.homepage();
+		//homepage.assertLinkIsVisibleAndClickIt(browser, "UITEST: Healthy weight");
 
-        var topicpage = browser.page.healthystockport.topicpage();
-        topicpage.assertTitleIsVisible('UITEST: Healthy weight');
-        topicpage.goToArticlePage(browser, "UITEST: A balanced diet", "uitest-a-balanced-diet");
+  //      var topicpage = browser.page.healthystockport.topicpage();
+  //      topicpage.assertTitleIsVisible('UITEST: Healthy weight');
+  //      topicpage.goToArticlePage(browser, "UITEST: A balanced diet", "uitest-a-balanced-diet");
 
 
-        var articlepage = browser.page.healthystockport.articlepage();
-        articlepage.assertTitleIsVisible("UITEST: Getting started");
-        articlepage.assertProfileIsVisible("Brinnington, Stockport");
-        articlepage.goToNextSection(browser);
+  //      var articlepage = browser.page.healthystockport.articlepage();
+  //      articlepage.assertTitleIsVisible("UITEST: Getting started");
+  //      articlepage.assertProfileIsVisible("Brinnington, Stockport");
+  //      articlepage.goToNextSection(browser);
 
         //articlepage.goToContactUsPage(browser);
 
@@ -27,7 +27,7 @@ module.exports = {
         //contactuspage.goToHomePage(browser);
 
         //homepage.assertTitleIsVisible("People of Stockport");
-    },
+    //},
    
     after: function (browser, done) {
         setTimeout(function () {
