d53d721a727c64b00827f7dab93e2ea252a29c32
Zico Harris <zico.harris@stockport.gov.uk>
2018-02-02 09:59:35 +0000

    Zico - Report inappropriate group confirmation page changes

--- src/StockportWebapp/Controllers/GroupsController.cs
+++ src/StockportWebapp/Controllers/GroupsController.cs
@@ -413,11 +413,11 @@ namespace StockportWebapp.Controllers
                 Title = "Changes to a group's information",
                 SubTitle = $"You've successfully submitted a change for {groupName}",
                 ConfirmationText = "We will take a look at the changes you have suggested so that we can make sure that they are correct.",
-                ButtonText = "Go back to manage your groups",
-                ButtonLink = Url.Action("manage"),
-                Icon = "check",
-                IconColour = "green",
-                Crumbs = new List<Crumb> { new Crumb("Stockport Local", "groups", "Group"), new Crumb(ViewBag.GroupName, ViewBag.Slug, "groups") }
+				ButtonText = $"Go back to Stockport Local {groupName}",
+				ButtonLink = "/groups/" + slug,
+				Icon = "check",
+				IconColour = "green",
+				Crumbs = new List<Crumb> { new Crumb("Stockport Local", "groups", "Group"), new Crumb(ViewBag.GroupName, ViewBag.Slug, "groups") }
             };
 
             return View("Confirmation", viewmodel);
