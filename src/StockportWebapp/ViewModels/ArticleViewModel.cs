9881765c73977150eaacb92344c5efe1568fa3aa
David Meek <david.meek@stockport.gov.uk>
2018-05-16 11:45:27 +0100

    David #907 - Removing tertiary items from article sidebar

--- src/StockportWebapp/ViewModels/ArticleViewModel.cs
+++ src/StockportWebapp/ViewModels/ArticleViewModel.cs
@@ -104,7 +104,6 @@ namespace StockportWebapp.ViewModels
             var sidebarSubItems = new List<SubItem>();
             sidebarSubItems.AddRange(parentTopic.SubItems);
             sidebarSubItems.AddRange(parentTopic.SecondaryItems);
-            sidebarSubItems.AddRange(parentTopic.TertiaryItems);
             hasMoreButton = sidebarSubItems.Count > 6;
             return sidebarSubItems.Take(6);
         }
