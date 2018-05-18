b6e40c8bc0774fbac775d811c98327552f427603
Lydia Gray <lydia.gray@stockport.gov.uk>
2018-05-18 11:21:33 +0100

    David/Lydia - added tag parser tests for PrivacyNoticeTagParser and SectionTagParser

---
+++ test/StockportWebappTests/Unit/Parsers/PrivacyNoticeTagParserTest.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Amazon.Runtime.Internal.Util;
+using Moq;
+using Xunit;
+using FluentAssertions;
+using StockportWebapp.Utils;
+using StockportWebapp.Parsers;
+using Microsoft.Extensions.Logging;
+using StockportWebapp.Models;
+
+namespace StockportWebappTests.Unit.Parsers
+{
+    public class PrivacyNoticeTagParserTest
+    {
+        private readonly Mock<IViewRender> _viewRenderer;
+        private readonly PrivacyNoticeTagParser _privacyNoticeTagParser;
+        private readonly Mock<ILogger<PrivacyNotice>> _mockLogger;
+
+        public PrivacyNoticeTagParserTest()
+        {
+            _viewRenderer = new Mock<IViewRender>();
+            _mockLogger = new Mock<ILogger<PrivacyNotice>>();
+            _privacyNoticeTagParser = new PrivacyNoticeTagParser(_viewRenderer.Object, _mockLogger.Object);
+        }
+
+        [Fact]
+        public void Parse_ShouldRenderIfPrivacyExists()
+        {
+            //arrange
+            var content = "{{PrivacyNotice:title}}";
+            var privacyNotice = new List<PrivacyNotice>()
+            {
+                new PrivacyNotice()
+                {
+                    Title = "title",
+                    Slug = "slug"
+                }
+            };
+            var renderResult = "";
+            _viewRenderer.Setup(o => o.Render("PrivacyNotice", It.IsAny<IEnumerable<PrivacyNotice>>())).Returns(renderResult);
+            //act
+            _privacyNoticeTagParser.Parse(content, privacyNotice);
+            //assert
+            _viewRenderer.Verify(o => o.Render("PrivacyNotice", It.IsAny<IEnumerable<PrivacyNotice>>()), Times.Once);
+        }
+
+        [Fact]
+        public void Parse_ShouldNotRenderIfPrivacyNoticeDoesNotExist()
+        {
+            //arrange
+            var content = "{{PrivacyNotice:category}}";
+            var privacyNotice = new List<PrivacyNotice>()
+            {
+                new PrivacyNotice()
+                {
+                    Title = "title",
+                    Slug = "slug"
+                }
+            };
+            var renderResult = "";
+            _viewRenderer.Setup(o => o.Render("PrivacyNotice", It.IsAny<IEnumerable<PrivacyNotice>>())).Returns(renderResult);
+            //act
+            _privacyNoticeTagParser.Parse(content, privacyNotice);
+            //assert
+            _viewRenderer.Verify(o => o.Render("PrivacyNotice", It.IsAny<IEnumerable<PrivacyNotice>>()), Times.Never);
+        }
+
+        [Fact]
+        public void Parse_ShouldReplaceContentIfPrivacyNoticeExists()
+        {
+            //arrange
+            var content = "{{PrivacyNotice:title}}";
+            var privacyNotice = new List<PrivacyNotice>()
+            {
+                new PrivacyNotice()
+                {
+                    Title = "title",
+                    Slug = "slug"
+                }
+            };
+            var renderResult = "<h1>title</h1>";
+            _viewRenderer.Setup(o => o.Render("PrivacyNotice", It.IsAny<IEnumerable<PrivacyNotice>>())).Returns(renderResult);
+            //act
+            var result = _privacyNoticeTagParser.Parse(content, privacyNotice);
+            //assert
+            result.Should().Be("<h1>title</h1>");
+        }
+    }
+}
