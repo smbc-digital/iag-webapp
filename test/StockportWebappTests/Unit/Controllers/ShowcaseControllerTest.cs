7c14dd510403150e8307ba8a8a1e425158b11ad2
Elliott Humphreys <elliott.humphreys@stockport.gov.uk>
2019-01-14 14:57:47 +0000

    fix(Showcase): refactored showcase to use new model

--- test/StockportWebappTests/Unit/Controllers/ShowcaseControllerTest.cs
+++ test/StockportWebappTests/Unit/Controllers/ShowcaseControllerTest.cs
@@ -14,17 +14,19 @@ using StockportWebapp.ProcessedModels;
 using System;
 using System.Threading.Tasks;
 using StockportWebapp.Repositories;
+using StockportWebapp.FeatureToggling;
 
 namespace StockportWebappTests_Unit.Unit.Controllers
 {
     public class ShowcaseControllerTest
     {
         private readonly ShowcaseController _controller;
+        private readonly Mock<FeatureToggles> _mockFeatureToggles = new Mock<FeatureToggles>();
         private readonly Mock<IProcessedContentRepository> _mockContentRepository = new Mock<IProcessedContentRepository>();
 
         public ShowcaseControllerTest()
         {
-            _controller = new ShowcaseController(_mockContentRepository.Object, new Mock<ILogger<ShowcaseController>>().Object, new Mock<IApplicationConfiguration>().Object);
+            _controller = new ShowcaseController(_mockContentRepository.Object, new Mock<ILogger<ShowcaseController>>().Object, new Mock<IApplicationConfiguration>().Object, _mockFeatureToggles.Object);
         }
 
         [Fact]
@@ -35,7 +37,7 @@ namespace StockportWebappTests_Unit.Unit.Controllers
                                                                  new DateTime(9999, 9, 9, 0, 0, 0, DateTimeKind.Utc), string.Empty)};
             var showcase = new ProcessedShowcase("Test showcase", showcaseSlug, "showcase teaser",
                                                  "showcase subheading", "event category", "events Category Or Tag", "event subheading", "news subheading", "news category", "news type", "body subheading", "body", null, "af981b9771822643da7a03a9ae95886f/picture.jpg",
-                                                 new List<SubItem> { new SubItem("slug", "title", "teaser", "icon", "type", "image.jpg", new List<SubItem>()) }, new List<Crumb> { new Crumb("title", "slug", "type") }, new List<Consultation>(), new List<SocialMediaLink>(), new List<Event>(), "", "", alerts, new List<SubItem>(), null, null, new FieldOrder(),"Key Fact Subheading", "fa-icon");
+                                                 new List<SubItem> { new SubItem("slug", "title", "teaser", "icon", "type", "image.jpg", new List<SubItem>()) }, new List<Crumb> { new Crumb("title", "slug", "type") }, new List<Consultation>(), new List<SocialMediaLink>(), new List<Event>(), "", "", alerts, new List<SubItem>(), null, null, new FieldOrder(),"Key Fact Subheading", "fa-icon", new List<DidYouKnow>());
 
             _mockContentRepository
                 .Setup(_ => _.Get<Showcase>(It.IsAny<string>(), It.IsAny<List<Query>>()))
@@ -51,7 +53,7 @@ namespace StockportWebappTests_Unit.Unit.Controllers
             processedShowcase.EventCategory.Should().Be("event category");
             processedShowcase.EventSubheading.Should().Be("event subheading");
             processedShowcase.Breadcrumbs.Count().Should().Be(1);
-            processedShowcase.FeaturedItems.Count().Should().Be(1);
+            processedShowcase.SecondaryItems.Count().Should().Be(1);
             processedShowcase.Alerts.Count().Should().Be(1);
             processedShowcase.KeyFactSubheading.Should().Be("Key Fact Subheading");
             processedShowcase.Icon.Should().Be("fa-icon");
