68e08b234456fb5231b2ce0ac9da418a2ad89cf4
Jon Chiles <jonchiles@yahoo.com>
2019-01-29 10:06:31 +0000

    Articles (Chore) Removed cache from Articles so we can update snow notification page quicker

--- src/StockportWebapp/Controllers/ArticleController.cs
+++ src/StockportWebapp/Controllers/ArticleController.cs
@@ -13,7 +13,7 @@ using Microsoft.ApplicationInsights.AspNetCore.Extensions;
 
 namespace StockportWebapp.Controllers
 {
-    [ResponseCache(Location = ResponseCacheLocation.Any, Duration = Cache.Short)]
+    //[ResponseCache(Location = ResponseCacheLocation.Any, Duration = Cache.Short)]
     public class ArticleController : Controller
     {
         private readonly IProcessedContentRepository _repository;
