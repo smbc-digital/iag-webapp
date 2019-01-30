67629b678eab40c1d7bb830163a0f211b082aa35
Elliott Humphreys <elliott.humphreys@stockport.gov.uk>
2019-01-30 12:50:15 +0000

    fix(Profile): removed profile factort tests

---
+++ src/StockportWebapp/Extensions/IEnumerableExtension.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace StockportWebapp.Extensions
+{
+    public static class IEnumerableExtension
+    {
+        public static bool AnyOrDefault<T>(this IEnumerable<T> source, bool defaultValue = false)
+        {
+            if (source == null)
+                return defaultValue;
+
+            return source.Any();
+        }
+    }
+}
