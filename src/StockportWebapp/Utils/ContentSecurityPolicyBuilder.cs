4c1cb37cf61938a154cad97699475636e7c992d1
Elliott Humphreys <elliott.humphreys@stockport.gov.uk>
2019-03-21 13:26:25 +0000

    fix(CSP): added *.stockport.gov.uk to connect-src within CSP to allow Jquery to load an iframe of myaccount for AMI

--- src/StockportWebapp/Utils/ContentSecurityPolicyBuilder.cs
+++ src/StockportWebapp/Utils/ContentSecurityPolicyBuilder.cs
@@ -50,8 +50,8 @@ namespace StockportWebapp.Utils
 				.AddSource("http://stage.midas-pps.tractivity.co.uk/")
 				.AddSource("*.cloudfront.net/butotv/live/", false, true)
 				.AddSource("https://www.facebook.com/")
-				.AddSource("https://myaccount.stockport.gov.uk")
-				.Finish());
+				.AddSource("*.stockport.gov.uk")
+                .Finish());
 		}
 
 		private void BuildFontSource()
@@ -206,6 +206,7 @@ namespace StockportWebapp.Utils
 				.AddSource("wss://chat.meetami.ai/", false, true)
                 .AddSource("wss://chat.meetami.ai/socket.io/", false, true)
                 .AddSource("http://localhost/sitereplier/chats/enabled/")
+                .AddSource("*.stockport.gov.uk")
 				.Finish());
 		}
 
