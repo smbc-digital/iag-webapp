02f966d19bf1654a235e4fa5e3f484408f65264e
Jon Chiles <jonchiles@yahoo.com>
2019-09-03 15:26:26 +0100

    Chore(SmartAnswers): deleted old commented out text

--- src/StockportWebapp/QuestionBuilder/BaseQuestionController.cs
+++ src/StockportWebapp/QuestionBuilder/BaseQuestionController.cs
@@ -222,68 +222,6 @@ namespace StockportWebapp.QuestionBuilder
             return View(result);
         }
 
-        //private string checkSpecialText(string description, IList<Answer> prevAnswers)
-        //{
-        //    var openBoi = description.Count(x => x == '{');
-        //    var closedBoi = description.Count(x => x == '}');
-        //    if (openBoi != closedBoi)
-        //    {
-        //        return description;
-        //    }
-
-        //    if (openBoi > 1)
-        //    {
-        //        var descSpilt = description.Split('}');
-        //        descSpilt = descSpilt.Take(descSpilt.Count() - 1).ToArray();
-
-        //        foreach (var decSplitRow in descSpilt)
-        //        {
-        //            var indDescSplit = decSplitRow.Replace("{", "").Replace("}", "").Split(':');
-
-        //            foreach (Answer answer in prevAnswers)
-        //            {
-        //                if (answer.QuestionId == indDescSplit[0] && answer.Response == indDescSplit[1])
-        //                {
-        //                    description = indDescSplit[2];
-        //                    return description;
-        //                }
-        //            }
-
-        //        }
-        //        return description;
-        //    }
-        //    else
-        //    {
-        //        var indDescSplit = description.Replace("{", "").Replace("}", "").Split(':');
-        //        if (indDescSplit.Length == 2)
-        //        {
-        //            foreach (Answer answer in prevAnswers)
-        //            {
-        //                if (answer.QuestionId == indDescSplit[1].Trim())
-        //                {
-        //                    description = indDescSplit[0] + ": " + answer.Response;
-        //                    return description;
-        //                }
-        //            }
-        //            return description;
-        //        }
-        //        else
-        //        {
-        //            foreach (Answer answer in prevAnswers)
-        //            {
-        //                if (answer.QuestionId == indDescSplit[0] && answer.Response == indDescSplit[1])
-        //                {
-        //                    description = indDescSplit[2];
-        //                    return description;
-        //                }
-        //            }
-        //            return description;
-
-        //        }
-
-        //    }
-        //}
-
         public Page GetPage(int pageId)
         {
             var page = new Page();
