40955485bf85a4d438f29b93cb05e5e17a36c0c9
Lydia Gray <lydia.gray@stockport.gov.uk>
2020-01-28 13:18:08 +0000

    chore(UnitTests): add tests for TwentyThree in Video tag parser

--- test/StockportWebappTests/Unit/Parsers/VideoTagParserTest.cs
+++ test/StockportWebappTests/Unit/Parsers/VideoTagParserTest.cs
@@ -18,7 +18,7 @@ namespace StockportWebappTests_Unit.Unit.Parsers
         }
 
         [Fact]
-        public void ShouldParseVideoTags()
+        public void ShouldParseButoVideoTags()
         {
             var tag = "a-video-tag";
             var response = _parser.Parse("{{VIDEO:" + tag + "}}");
@@ -38,6 +38,26 @@ namespace StockportWebappTests_Unit.Unit.Parsers
             outputHtml.Append($"}})(document, {{\"object_id\":\"{tag}\", \"width\": \"100%\", \"height\": \"100%\"}})");
             outputHtml.Append("</script>");
             outputHtml.Append("</div>");
+
+            response.Should().Be(outputHtml.ToString());
+        }
+
+        [Fact]
+        public void ShouldParseTwentyThreeVideoTags()
+        {
+            _featureToggles.Object.TwentyThreeVideo = true;
+            var tag = "a-video-tag";
+            var response = _parser.Parse("{{VIDEO:" + tag + "}}");
+
+            var outputHtml = new StringBuilder();
+
+            outputHtml.Append("<div class=\"video-wrapper\">");
+            outputHtml.Append("<iframe src=");
+            outputHtml.Append("\"https://y84kj.videomarketingplatform.co/v.ihtml/player.html?source=embed&");
+            outputHtml.Append($"photo%5fid={tag}\" style=\"width:100%; height:100%; position:absolute; top:0; left:0;\" ");
+            outputHtml.Append("frameborder=\"0\" border=\"0\" scrolling=\"no\" allowfullscreen=\"1\" mozallowfullscreen=\"1\" ");
+            outputHtml.Append("webkitallowfullscreen=\"1\" allow=\"autoplay; fullscreen\">");
+            outputHtml.Append("</iframe></div>");
 
             response.Should().Be(outputHtml.ToString());
         }
