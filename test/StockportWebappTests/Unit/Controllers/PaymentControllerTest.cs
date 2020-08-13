65bc405767dabc7b6d16656612d5f0304aa4e0dd
Lydia Gray <lydia.gray@stockport.gov.uk>
2020-08-13 15:19:01 +0100

    chore(PaymentControllerTest): add unit tests

--- test/StockportWebappTests/Unit/Controllers/PaymentControllerTest.cs
+++ test/StockportWebappTests/Unit/Controllers/PaymentControllerTest.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 using System.Net;
 using FluentAssertions;
 using Microsoft.AspNetCore.Mvc;
@@ -9,6 +10,7 @@ using HttpResponse = StockportWebapp.Http.HttpResponse;
 using StockportWebapp.ProcessedModels;
 using Moq;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc.ModelBinding.Validation;
 using StockportWebapp.Enums;
 using StockportWebapp.Repositories;
 using StockportWebappTests_Unit.Helpers;
@@ -78,9 +80,164 @@ namespace StockportWebappTests_Unit.Unit.Controllers
                 .Setup(_ => _.Get<Payment>(It.IsAny<string>(), It.IsAny<List<Query>>()))
                 .ReturnsAsync(new HttpResponse((int)HttpStatusCode.OK, processedPayment, string.Empty));
 
-            await _paymentController.Detail("slug", null, null);
+            _configuration.Setup(_ => _.GetSection(It.IsAny<string>()))
+                .Returns(new Mock<IConfigurationSection>().Object);
+
+            var objectValidator = new Mock<IObjectModelValidator>();
+            objectValidator.Setup(o => o.Validate(It.IsAny<ActionContext>(),
+                It.IsAny<ValidationStateDictionary>(),
+                It.IsAny<string>(),
+                It.IsAny<Object>()));
+            _paymentController.ObjectValidator = objectValidator.Object;
+
+            await _paymentController.Detail("slug", new PaymentSubmission
+            {
+                Payment = new ProcessedPayment("title", "slug", "teaser", "description", "payDetailsText", "refLabel", "fund", "glCode", null, EPaymentReferenceValidation.None, "meta", "returnUrl", "catId", "accRef", "payDesc", null),
+                Amount = 12.00m,
+                Reference = "123456789"
+            });
 
             _civicaPayGateway.Verify(_ => _.CreateImmediateBasketAsync(It.IsAny<CreateImmediateBasketRequest>()), Times.Once);
+        }
+
+        [Fact]
+        public async Task DetailPostShouldReturnDetailsViewIfModelStateInvalid()
+        {
+            var processedPayment = new ProcessedPayment(TextHelper.AnyString, TextHelper.AnyString, TextHelper.AnyString, TextHelper.AnyString,
+                TextHelper.AnyString, TextHelper.AnyString, TextHelper.AnyString, TextHelper.AnyString, new List<Crumb>(),
+                EPaymentReferenceValidation.None, TextHelper.AnyString, TextHelper.AnyString, TextHelper.AnyString, TextHelper.AnyString, TextHelper.AnyString, new List<Alert>());
+
+            _fakeRepository
+                .Setup(_ => _.Get<Payment>(It.IsAny<string>(), It.IsAny<List<Query>>()))
+                .ReturnsAsync(new HttpResponse((int)HttpStatusCode.OK, processedPayment, string.Empty));
+
+            var objectValidator = new Mock<IObjectModelValidator>();
+            objectValidator.Setup(o => o.Validate(It.IsAny<ActionContext>(),
+                It.IsAny<ValidationStateDictionary>(),
+                It.IsAny<string>(),
+                It.IsAny<Object>()));
+            _paymentController.ObjectValidator = objectValidator.Object;
+
+            _paymentController.ModelState.AddModelError("Reference", "error");
+
+            var result = await _paymentController.Detail("slug", new PaymentSubmission
+            {
+                Payment = new ProcessedPayment("title", "slug", "teaser", "description", "payDetailsText", "refLabel", "fund", "glCode", null, EPaymentReferenceValidation.None, "meta", "returnUrl", "catId", "accRef", "payDesc", null),
+                Amount = 12.00m,
+                Reference = "123456"
+            }) as ViewResult;
+
+            _civicaPayGateway.Verify(_ => _.CreateImmediateBasketAsync(It.IsAny<CreateImmediateBasketRequest>()), Times.Never);
+            result.Should().BeOfType<ViewResult>();
+        }
+
+        [Fact]
+        public async Task DetailPostShouldReturnErrorViewIfBadRequest()
+        {
+            _civicaPayGateway.Setup(_ => _.CreateImmediateBasketAsync(It.IsAny<CreateImmediateBasketRequest>()))
+                .ReturnsAsync(new HttpResponse<CreateImmediateBasketResponse> { StatusCode = HttpStatusCode.BadRequest, ResponseContent = new CreateImmediateBasketResponse { BasketReference = "testRef", BasketToken = "testBasketToken", ResponseCode = "00000" } });
+
+            var processedPayment = new ProcessedPayment(TextHelper.AnyString, TextHelper.AnyString, TextHelper.AnyString, TextHelper.AnyString,
+                TextHelper.AnyString, TextHelper.AnyString, TextHelper.AnyString, TextHelper.AnyString, new List<Crumb>(),
+                EPaymentReferenceValidation.None, TextHelper.AnyString, TextHelper.AnyString, TextHelper.AnyString, TextHelper.AnyString, TextHelper.AnyString, new List<Alert>());
+
+            _fakeRepository
+                .Setup(_ => _.Get<Payment>(It.IsAny<string>(), It.IsAny<List<Query>>()))
+                .ReturnsAsync(new HttpResponse((int)HttpStatusCode.OK, processedPayment, string.Empty));
+
+            _configuration.Setup(_ => _.GetSection(It.IsAny<string>()))
+                .Returns(new Mock<IConfigurationSection>().Object);
+
+            var objectValidator = new Mock<IObjectModelValidator>();
+            objectValidator.Setup(o => o.Validate(It.IsAny<ActionContext>(),
+                It.IsAny<ValidationStateDictionary>(),
+                It.IsAny<string>(),
+                It.IsAny<Object>()));
+            _paymentController.ObjectValidator = objectValidator.Object;
+
+            var result = await _paymentController.Detail("slug", new PaymentSubmission
+            {
+                Payment = new ProcessedPayment("title", "slug", "teaser", "description", "payDetailsText", "refLabel", "fund", "glCode", null, EPaymentReferenceValidation.None, "meta", "returnUrl", "catId", "accRef", "payDesc", null),
+                Amount = 12.00m,
+                Reference = "123456789"
+            }) as ViewResult;
+
+            _civicaPayGateway.Verify(_ => _.CreateImmediateBasketAsync(It.IsAny<CreateImmediateBasketRequest>()), Times.Once);
+            result.Should().BeOfType<ViewResult>();
+            result.ViewName.Should().Be("Error");
+        }
+
+        [Fact]
+        public async Task DetailPostShouldReturnViewIfResponseCodeIs00001()
+        {
+            _civicaPayGateway.Setup(_ => _.CreateImmediateBasketAsync(It.IsAny<CreateImmediateBasketRequest>()))
+                .ReturnsAsync(new HttpResponse<CreateImmediateBasketResponse> { StatusCode = HttpStatusCode.BadRequest, ResponseContent = new CreateImmediateBasketResponse { BasketReference = "testRef", BasketToken = "testBasketToken", ResponseCode = "00001" } });
+
+            var processedPayment = new ProcessedPayment(TextHelper.AnyString, TextHelper.AnyString, TextHelper.AnyString, TextHelper.AnyString,
+                TextHelper.AnyString, TextHelper.AnyString, TextHelper.AnyString, TextHelper.AnyString, new List<Crumb>(),
+                EPaymentReferenceValidation.None, TextHelper.AnyString, TextHelper.AnyString, TextHelper.AnyString, TextHelper.AnyString, TextHelper.AnyString, new List<Alert>());
+
+            _fakeRepository
+                .Setup(_ => _.Get<Payment>(It.IsAny<string>(), It.IsAny<List<Query>>()))
+                .ReturnsAsync(new HttpResponse((int)HttpStatusCode.OK, processedPayment, string.Empty));
+
+            _configuration.Setup(_ => _.GetSection(It.IsAny<string>()))
+                .Returns(new Mock<IConfigurationSection>().Object);
+
+            var objectValidator = new Mock<IObjectModelValidator>();
+            objectValidator.Setup(o => o.Validate(It.IsAny<ActionContext>(),
+                It.IsAny<ValidationStateDictionary>(),
+                It.IsAny<string>(),
+                It.IsAny<Object>()));
+            _paymentController.ObjectValidator = objectValidator.Object;
+
+            var result = await _paymentController.Detail("slug", new PaymentSubmission
+            {
+                Payment = new ProcessedPayment("title", "slug", "teaser", "description", "payDetailsText", "refLabel", "fund", "glCode", null, EPaymentReferenceValidation.None, "meta", "returnUrl", "catId", "accRef", "payDesc", null),
+                Amount = 12.00m,
+                Reference = "123456789"
+            }) as ViewResult;
+
+            _civicaPayGateway.Verify(_ => _.CreateImmediateBasketAsync(It.IsAny<CreateImmediateBasketRequest>()), Times.Once);
+            result.Should().BeOfType<ViewResult>();
+        }
+
+        [Fact]
+        public async Task DetailPostShouldCallGatewayGetPaymentUrl()
+        {
+            _civicaPayGateway.Setup(_ => _.CreateImmediateBasketAsync(It.IsAny<CreateImmediateBasketRequest>()))
+                .ReturnsAsync(new HttpResponse<CreateImmediateBasketResponse> { StatusCode = HttpStatusCode.OK, ResponseContent = new CreateImmediateBasketResponse { BasketReference = "testRef", BasketToken = "testBasketToken", ResponseCode = "00000" } });
+
+            _civicaPayGateway
+                .Setup(_ => _.GetPaymentUrl(It.IsAny<string>(), It.IsAny<string>(), It.IsAny<string>()))
+                .Returns("redirectUrl");
+
+            var processedPayment = new ProcessedPayment(TextHelper.AnyString, TextHelper.AnyString, TextHelper.AnyString, TextHelper.AnyString,
+                TextHelper.AnyString, TextHelper.AnyString, TextHelper.AnyString, TextHelper.AnyString, new List<Crumb>(),
+                EPaymentReferenceValidation.None, TextHelper.AnyString, TextHelper.AnyString, TextHelper.AnyString, TextHelper.AnyString, TextHelper.AnyString, new List<Alert>());
+
+            _fakeRepository
+                .Setup(_ => _.Get<Payment>(It.IsAny<string>(), It.IsAny<List<Query>>()))
+                .ReturnsAsync(new HttpResponse((int)HttpStatusCode.OK, processedPayment, string.Empty));
+
+            _configuration.Setup(_ => _.GetSection(It.IsAny<string>()))
+                .Returns(new Mock<IConfigurationSection>().Object);
+
+            var objectValidator = new Mock<IObjectModelValidator>();
+            objectValidator.Setup(o => o.Validate(It.IsAny<ActionContext>(),
+                It.IsAny<ValidationStateDictionary>(),
+                It.IsAny<string>(),
+                It.IsAny<Object>()));
+            _paymentController.ObjectValidator = objectValidator.Object;
+
+            await _paymentController.Detail("slug", new PaymentSubmission
+            {
+                Payment = new ProcessedPayment("title", "slug", "teaser", "description", "payDetailsText", "refLabel", "fund", "glCode", null, EPaymentReferenceValidation.None, "meta", "returnUrl", "catId", "accRef", "payDesc", null),
+                Amount = 12.00m,
+                Reference = "123456789"
+            });
+
+            _civicaPayGateway.Verify(_ => _.GetPaymentUrl(It.IsAny<string>(), It.IsAny<string>(), It.IsAny<string>()), Times.Once);
         }
     }
 }
