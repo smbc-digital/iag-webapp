f007d939474285feae49b0fb278e974b805883b8
Jordan <jordan.burns@stockport.gov.uk>
2020-12-10 14:43:13 +0000

    style(healthy stockport): Add underline to links

--- src/StockportWebapp/Middleware/BusinessIdMiddleware.cs
+++ src/StockportWebapp/Middleware/BusinessIdMiddleware.cs
@@ -27,9 +27,9 @@ namespace StockportWebapp.Middleware
             else
             {
                 // default to stockportgov if no businessid
-                businessId.SetId(new StringValues("stockportgov"));
+                //businessId.SetId(new StringValues("stockportgov"));
                 // to run healthystockport locally, comment out the above line and uncomment line below
-                //businessId.SetId(new StringValues("healthystockport"));
+                businessId.SetId(new StringValues("healthystockport"));
                 context.Request.Headers.Add("BUSINESS-ID", businessId.ToString());
 
                 if (context.Request.Path.HasValue 
