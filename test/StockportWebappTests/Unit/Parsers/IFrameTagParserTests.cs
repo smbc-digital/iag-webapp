f63d1821846bbbdd106a11bf269cf59b1ca11173
stephen.lally <stephen.lally.smbc@gmail.com>
2021-09-29 16:13:14 +0100

    tests: added and amended Video/IFrame tag parser tests

---
+++ test/StockportWebappTests/Unit/Parsers/IFrameTagParserTests.cs
@@ -0,0 +1,38 @@
+using FluentAssertions;
+using StockportWebapp.Parsers;
+using Xunit;
+
+namespace StockportWebappTests_Unit.Unit.Parsers
+{
+    public class IFrameTagParserTests
+    {
+        private readonly IFrameTagParser _parser;
+
+        public IFrameTagParserTests()
+        {
+            _parser = new IFrameTagParser();
+        }
+
+        [Fact]
+        public void Parse_Should_Parse_IFrame()
+        {
+            var tag = "https://www.stockport.gov.uk/";
+            var response = _parser.Parse("{{IFRAME:" + tag + "}}");
+
+            var outputHtml = $"<iframe  class='mapframe' allowfullscreen src='https://www.stockport.gov.uk/'></iframe>";
+
+            response.Should().Be(outputHtml);
+        }
+
+        [Fact]
+        public void Parse_Should_Parse_IFrame_WithOptional_Iframe_Title()
+        {
+            var tag = "https://www.stockport.gov.uk/;iframe optional title";
+            var response = _parser.Parse("{{IFRAME:" + tag + "}}");
+
+            var outputHtml = $"<iframe title=\"iframe optional title\" class='mapframe' allowfullscreen src='https://www.stockport.gov.uk/'></iframe>";
+
+            response.Should().Be(outputHtml);
+        }
+    }
+}
--- test/StockportWebappTests/Unit/Parsers/VideoTagParserTests.cs
+++ test/StockportWebappTests/Unit/Parsers/VideoTagParserTests.cs
@@ -5,11 +5,11 @@ using Xunit;
 
 namespace StockportWebappTests_Unit.Unit.Parsers
 {
-    public class VideoTagParserTest
+    public class VideoTagParserTests
     {
         private readonly VideoTagParser _parser;
 
-        public VideoTagParserTest()
+        public VideoTagParserTests()
         {
             _parser = new VideoTagParser();
         }
@@ -23,7 +23,26 @@ namespace StockportWebappTests_Unit.Unit.Parsers
             var outputHtml = new StringBuilder();
 
             outputHtml.Append("<div class=\"video-wrapper\">");
-            outputHtml.Append("<iframe src=");
+            outputHtml.Append("<iframe  src=");
+            outputHtml.Append("\"https://video.stockport.gov.uk/v.ihtml/player.html?token=VideoToken&source=embed&");
+            outputHtml.Append("photo%5fid=VideoId\" style=\"width:100%; height:100%; position:absolute; top:0; left:0;\" ");
+            outputHtml.Append("frameborder=\"0\" border=\"0\" scrolling=\"no\" allowfullscreen=\"1\" mozallowfullscreen=\"1\" ");
+            outputHtml.Append("webkitallowfullscreen=\"1\" allow=\"autoplay; fullscreen\">");
+            outputHtml.Append("</iframe></div>");
+
+            response.Should().Be(outputHtml.ToString());
+        }
+
+        [Fact]
+        public void ShouldParseTwentyThreeVideoTags_With_Optional_IFrameTitle()
+        {
+            var tag = "VideoId;VideoToken;iframe title";
+            var response = _parser.Parse("{{VIDEO:" + tag + "}}");
+
+            var outputHtml = new StringBuilder();
+
+            outputHtml.Append("<div class=\"video-wrapper\">");
+            outputHtml.Append("<iframe title=\"iframe title\" src=");
             outputHtml.Append("\"https://video.stockport.gov.uk/v.ihtml/player.html?token=VideoToken&source=embed&");
             outputHtml.Append("photo%5fid=VideoId\" style=\"width:100%; height:100%; position:absolute; top:0; left:0;\" ");
             outputHtml.Append("frameborder=\"0\" border=\"0\" scrolling=\"no\" allowfullscreen=\"1\" mozallowfullscreen=\"1\" ");
