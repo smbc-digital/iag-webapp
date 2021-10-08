a5951527fa724924f5ed60c8bfd6bdf73dff59a7
stephen.lally <stephen.lally.smbc@gmail.com>
2021-10-08 14:26:01 +0100

    fix: Added additional chars to iframe title for video

--- test/StockportWebappTests/Unit/Parsers/VideoTagParserTests.cs
+++ test/StockportWebappTests/Unit/Parsers/VideoTagParserTests.cs
@@ -51,5 +51,24 @@ namespace StockportWebappTests_Unit.Unit.Parsers
 
             response.Should().Be(outputHtml.ToString());
         }
+
+        [Fact]
+        public void ShouldParseTwentyThreeVideoTags_With_Optional_IFrameTitle_WhichHas_Different_Characters()
+        {
+            var tag = "VideoId;VideoToken;Title's # this+ ., Wow-Wow";
+            var response = _parser.Parse("{{VIDEO:" + tag + "}}");
+
+            var outputHtml = new StringBuilder();
+
+            outputHtml.Append("<div class=\"video-wrapper\">");
+            outputHtml.Append($"<iframe title=\"Title's # this+ ., Wow-Wow\" src=");
+            outputHtml.Append("\"https://video.stockport.gov.uk/v.ihtml/player.html?token=VideoToken&source=embed&");
+            outputHtml.Append("photo%5fid=VideoId\" style=\"width:100%; height:100%; position:absolute; top:0; left:0;\" ");
+            outputHtml.Append("frameborder=\"0\" border=\"0\" scrolling=\"no\" allowfullscreen=\"1\" mozallowfullscreen=\"1\" ");
+            outputHtml.Append("webkitallowfullscreen=\"1\" allow=\"autoplay; fullscreen\">");
+            outputHtml.Append("</iframe></div>");
+
+            response.Should().Be(outputHtml.ToString());
+        }
     }
 }
