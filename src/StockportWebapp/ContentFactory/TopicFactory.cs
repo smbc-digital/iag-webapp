997657011f555ed78967e6c608abcb21b0a05d07
Stu <h.s.j.wilkes@gmail.com>
2022-10-03 10:41:40 +0100

    feat(TriviaSection): add to Topic & HS view/style

--- src/StockportWebapp/ContentFactory/TopicFactory.cs
+++ src/StockportWebapp/ContentFactory/TopicFactory.cs
@@ -27,9 +27,10 @@ namespace StockportWebapp.ContentFactory
                 topic.EmailAlertsTopicId, topic.EventBanner, topic.ExpandingLinkTitle, topic.ExpandingLinkBoxes, topic.PrimaryItemTitle,
                 topic.Title, topic.DisplayContactUs, topic.CampaignBanner, topic.EventCategory)
             {
-                CallToAction = topic.CallToAction,
+                TriviaSection = topic.TriviaSection,
                 Video = !string.IsNullOrEmpty(topic.Video.VideoEmbedCode) ? 
-                    new Video(topic.Video.Heading, topic.Video.Text, _tagParserContainer.ParseAll(topic.Video.VideoEmbedCode)) : null
+                    new Video(topic.Video.Heading, topic.Video.Text, _tagParserContainer.ParseAll(topic.Video.VideoEmbedCode)) : null,
+                CallToAction = topic.CallToAction
             };
         }
     }
