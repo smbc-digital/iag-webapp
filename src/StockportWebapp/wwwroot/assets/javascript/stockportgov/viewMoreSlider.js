056ac475858769630ff8fd4561167076f8b607f0
Stu <h.s.j.wilkes@gmail.com>
2022-10-18 13:58:42 +0100

    fix9Accessibility): syntax edit

--- src/StockportWebapp/wwwroot/assets/javascript/stockportgov/viewMoreSlider.js
+++ src/StockportWebapp/wwwroot/assets/javascript/stockportgov/viewMoreSlider.js
@@ -1,24 +1,20 @@
 ﻿define(["jquery"], function ($) {
     return {
         Init: function () {
-            var container = $("#see-more-container");
+            var container = $("#see-more-container").hide();
             var button = $("#see-more-services");
-            container.hide();
+
             button.on("click", function () {
                 container.slideToggle(200, function () {
                     container.toggleClass("is-collapsed");
                     var oldButtonText = button.text();
                     if (oldButtonText.includes("fewer")) {
                         button.text(oldButtonText.replace('fewer', 'more'));
-                        $('html, body').animate({
-                            scrollTop: (button.offset().top - 150) + 'px'
-                        });
+                        $('html, body').animate({ scrollTop: (button.offset().top - 250) + 'px' });
                     }
                     else {
                         button.text(oldButtonText.replace('more', 'fewer'));
-                        if ($(".featured-topic-link")[8] !== undefined) {
-                            $(".featured-topic-link")[8].focus();
-                        }
+                        container.find(".featured-topic-link:first").focus();
                     }
                 });
             });
