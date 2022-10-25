86efbfc79f3c820760431c17d1429877d665c1f1
Stu <h.s.j.wilkes@gmail.com>
2022-10-25 08:46:05 +0100

    fix(Dependencies): rm references to packages

--- src/StockportWebapp/wwwroot/assets/javascript/requireConfig.js
+++ src/StockportWebapp/wwwroot/assets/javascript/requireConfig.js
@@ -3,16 +3,11 @@
     paths: {
         "jquery": "vendor/jquery.min",
         "recaptcha": "//www.google.com/recaptcha/api",
-        "bootstrap": "/lib/bootstrap/dist/js/bootstrap.min",
-        "handlebars": "/lib/handlebars/handlebars.runtime.min",
-        "rangy": "/lib/rangy-1.3/rangy-core",
         "validate": "vendor/jquery.validate.min",
         "unobtrusive": "vendor/jquery.validate.unobtrusive.min",
         "slick": "vendor/slick",
         "carousel": "stockportgov/carousel",
         "contactus": "stockportgov/contactUs",
-        "iag-validate-chars": "stockportgov/iagValidateChars",
-        "text-limiter": "stockportgov/textLimiter",
         "events": "stockportgov/events",
         "expandinglinks": "stockportgov/expandinglinks",
         "filters": "stockportgov/filters",
@@ -24,7 +19,6 @@
         "startup": "stockportgov/startup",
         "utils": "stockportgov/utils",
         "viewmoreslider": "stockportgov/viewmoreslider",
-        "clipboard": "//cdnjs.cloudflare.com/ajax/libs/clipboard.js/1.7.1/clipboard.min",
         "jquery-ui": "../stylesheets/vendor/jquery-ui-1.13.0.custom/jquery-ui.min",
         "favourites": "stockportgov/favourites",
         "tracking": "stockportgov/tracking",
@@ -33,8 +27,6 @@
         "jquery.steps": "/assets/javascript/vendor/jquery.steps.min",
         "multiStepForm": "stockportgov/multistep-form",
         "jquery.cookie": "/assets/javascript/vendor/jquery.cookie",
-        "trumbowyg": "/lib/trumbowyg/trumbowyg.min",
-        "trumbowyginit": "stockportgov/trumbowyginit",
         "reciteMe": "stockportgov/reciteme"
     },
     shim: {
@@ -47,16 +39,10 @@
         'jquery-ui': {
             deps: ['jquery']
         },
-        bootstrap: {
-            deps: ['jquery', 'jquery-ui']
-        },
         unobtrusive: {
             deps: ['validate']
         },
         validate: {
-            deps: ['jquery']
-        },
-        trumbowyg: {
             deps: ['jquery']
         }
     }
