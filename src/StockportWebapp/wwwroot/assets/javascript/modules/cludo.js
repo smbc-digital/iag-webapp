5e2b695a9a7d9c93f54ce7ac88f8bc008a5f2ae8
Stu <h.s.j.wilkes@gmail.com>
2022-11-11 12:11:34 +0000

    fix(require): IF healthy stockport to cludo init

---
+++ src/StockportWebapp/wwwroot/assets/javascript/modules/cludo.js
@@ -0,0 +1,33 @@
+﻿define(["jquery"], function () {
+    return {
+        Init: function (healthyStockport) {
+            var cludoSettings;
+            if (healthyStockport) {
+                cludoSettings = {
+                    customerId: 112,
+                    engineId: 1757,
+                    searchUrl: '/searchResults?query',
+                    language: 'en',
+                    searchInputs: ['cludo-search-form', 'cludo-search-mobile-form'],
+                    template: 'InlineBasic',
+                    focusOnResultsAfterSearch: true,
+                    type: 'inline'
+                };
+            } else {
+                // Stockport Gov
+                cludoSettings = {
+                    customerId: 112,
+                    engineId: 1144,
+                    searchUrl: '/searchResults?query',
+                    language: 'en',
+                    searchInputs: ['cludo-search-form', 'cludo-search-hero-form', 'cludo-search-mobile-form'],
+                    template: 'InlineBasic',
+                    focusOnResultsAfterSearch: true,
+                    type: 'inline'
+                };
+            }
+            CludoSearch = new Cludo(cludoSettings);
+            CludoSearch.init();
+        }
+    }
+});
