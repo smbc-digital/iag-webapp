595ff1db5965b3f57f0a3cb30d571a0fcbda9f76
Michael Hayes <michael.hayes@stockport.gov.uk>
2022-11-14 15:05:50 +0000

    chore(parser): add other sandbox params to form parser

--- src/StockportWebapp/Parsers/FormBuilderTagParser.cs
+++ src/StockportWebapp/Parsers/FormBuilderTagParser.cs
@@ -23,7 +23,7 @@ namespace StockportWebapp.Parsers
             if (splitTagData.Length > 1)
                 iFrameTitle = $"title=\"{splitTagData[1]}\"";
 
-            return $"<iframe sandbox='allow-scripts allow-forms' {iFrameTitle} class='mapframe' allowfullscreen src='{splitTagData[0]}'></iframe>";
+            return $"<iframe sandbox='allow-forms allow-scripts allow-top-navigation-by-user-activation allow-same-origin' {iFrameTitle} class='mapframe' allowfullscreen src='{splitTagData[0]}'></iframe>";
         }
         
         public FormBuilderTagParser()
