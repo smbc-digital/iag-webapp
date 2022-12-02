e82efc87ae3b808a557834c5d57cc8f673a852c1
Stu <h.s.j.wilkes@gmail.com>
2022-12-02 07:58:31 +0000

    feat(NET6): Dependencies > WebApp

--- src/StockportWebapp/Program.cs
+++ src/StockportWebapp/Program.cs
@@ -1,28 +1,38 @@
-﻿using Microsoft.AspNetCore;
+﻿var builder = WebApplication.CreateBuilder(args);
 
-namespace StockportWebapp
-{
-    public class Program
+builder.Configuration.SetBasePath(builder.Environment.ContentRootPath + "/app-config");
+builder.Configuration
+    .AddJsonFile("appsettings.json")
+    .AddJsonFile($"appsettings.{builder.Environment.EnvironmentName}.json")
+    .AddJsonFile("appversion.json", true)
+    .AddJsonFile($"{builder.Configuration.GetSection("secrets-location").Value}/appsettings.{builder.Environment.EnvironmentName}.secrets.json");
+
+builder.Host.UseSerilog((context, services, configuration) => configuration
+    .ReadFrom.Configuration(context.Configuration)
+    .WriteToElasticsearchAws(builder.Configuration));
+
+var startup = new Startup(builder.Configuration, builder.Environment);
+startup.ConfigureServices(builder.Services);
+
+var app = builder.Build();
+
+if (!app.Environment.IsEnvironment("prod") && !app.Environment.IsEnvironment("stage"))
+    app.UseDeveloperExceptionPage();
+
+app.UseSerilogRequestLogging();
+
+app.UseMiddleware<BusinessIdMiddleware>()
+    .UseMiddleware<ShortUrlRedirectsMiddleware>()
+    .UseMiddleware<RobotsMiddleware>()
+    .UseMiddleware<SecurityHeaderMiddleware>()
+    .UseStatusCodePagesWithReExecute("/error")
+    .UseCustomStaticFiles()
+    .UseCustomCulture()
+    .UseRouting()
+    .Map("/favicon.ico", Startup.HandleFaviconRequests)
+    .UseEndpoints(endpoints =>
     {
-        public static void Main(string[] args)
-        {
-            CreateWebHostBuilder(args).Build().Run();
-        }
+        endpoints.MapControllers();
+    });
 
-        public static IWebHostBuilder CreateWebHostBuilder(string[] args) =>
-            WebHost.CreateDefaultBuilder<Startup>(args)
-                .ConfigureAppConfiguration((hostContext, config) =>
-                {
-                    config.Sources.Clear();
-                    config.SetBasePath(hostContext.HostingEnvironment.ContentRootPath + "/app-config");
-                    config
-                        .AddJsonFile("appsettings.json")
-                        .AddJsonFile($"appsettings.{hostContext.HostingEnvironment.EnvironmentName}.json")
-                        .AddJsonFile("appversion.json", true);
-                    var tempConfig = config.Build();
-                    config.AddJsonFile(
-                        $"{tempConfig["secrets-location"]}/appsettings.{hostContext.HostingEnvironment.EnvironmentName}.secrets.json");
-                });
-
-    }
-}
+app.Run();
--- src/StockportWebapp/Startup.cs
+++ src/StockportWebapp/Startup.cs
@@ -1,16 +1,10 @@
 ﻿using Microsoft.Extensions.Primitives;
-using Newtonsoft.Json;
 using Newtonsoft.Json.Serialization;
-using Serilog;
 using StockportGovUK.NetStandard.Gateways;
 using StockportWebapp.Config;
-using StockportWebapp.Extensions;
-using StockportWebapp.Middleware;
-using StockportWebapp.ModelBinders;
 using StockportWebapp.Models;
 using StockportWebapp.Utils;
 using StockportWebapp.Wrappers;
-using ILogger = Serilog.ILogger;
 
 namespace StockportWebapp
 {
@@ -32,12 +26,8 @@ namespace StockportWebapp
             _sendAmazonEmails = string.IsNullOrEmpty(Configuration["SendAmazonEmails"]) || Configuration["SendAmazonEmails"] == "true";
         }
 
-        private ILogger StartupLogger { get; set; }
-
         public virtual void ConfigureServices(IServiceCollection services)
         {
-            ConfigureSerilog();
-
             services.AddControllersWithViews(options =>
             {
                 options.ModelBinderProviders.Insert(0, new DateTimeFormatConverterModelBinderProvider());
@@ -61,7 +51,7 @@ namespace StockportWebapp
             services.AddTransient<IUrlGeneratorSimple>(p => new UrlGeneratorSimple(p.GetService<IApplicationConfiguration>(), p.GetService<BusinessId>()));
             services.AddSingleton<IStaticAssets, StaticAssets>();
             services.AddTransient<IFilteredUrl>(p => new FilteredUrl(p.GetService<ITimeProvider>()));
-            services.AddTransient<IHttpClientWrapper>(provider => new HttpClientWrapper(new System.Net.Http.HttpClient(), provider.GetService<ILogger<HttpClientWrapper>>()));
+            services.AddTransient<IHttpClientWrapper>(provider => new HttpClientWrapper(new HttpClient(), provider.GetService<ILogger<HttpClientWrapper>>()));
 
             // custom extensions
             services.AddCustomisedAngleSharp();
@@ -79,44 +69,13 @@ namespace StockportWebapp
             services.AddCustomServices(_contentRootPath, _appEnvironmentName);
             services.AddBuilders();
             services.AddHelpers();
-            services.AddGroupConfiguration(Configuration, StartupLogger);
-            services.AddSesEmailConfiguration(Configuration, StartupLogger);
-            services.AddRedis(Configuration, _useRedisSession, StartupLogger);
+            services.AddGroupConfiguration(Configuration, Log.Logger);
+            services.AddSesEmailConfiguration(Configuration, Log.Logger);
+            services.AddRedis(Configuration, _useRedisSession, Log.Logger);
             services.AddResilientHttpClients<IGateway, Gateway>(Configuration);
-
-            services.AddApplicationInsightsTelemetry(Configuration);
         }
 
-        public void Configure(
-            IApplicationBuilder app,
-            IWebHostEnvironment env,
-            ILoggerFactory loggerFactory,
-            IHostApplicationLifetime appLifetime)
-        {
-            loggerFactory.AddSerilog();
-
-            if (!env.IsEnvironment("prod") && !env.IsEnvironment("stage"))
-                app.UseDeveloperExceptionPage();
-
-            app.UseMiddleware<BusinessIdMiddleware>()
-                .UseMiddleware<ShortUrlRedirectsMiddleware>()
-                .UseMiddleware<RobotsMiddleware>()
-                .UseMiddleware<SecurityHeaderMiddleware>()
-                .UseStatusCodePagesWithReExecute("/error")
-                .UseCustomStaticFiles()
-                .UseCustomCulture()
-                .UseRouting()
-                .Map("/favicon.ico", HandleFaviconRequests)
-                .UseEndpoints(endpoints =>
-                {
-                    endpoints.MapControllers();
-                });
-
-            // Close logger
-            appLifetime.ApplicationStopped.Register(Log.CloseAndFlush);
-        }
-
-        private static void HandleFaviconRequests(IApplicationBuilder app)
+        public static void HandleFaviconRequests(IApplicationBuilder app)
         {
             app.Run(context =>
             {
@@ -129,21 +88,6 @@ namespace StockportWebapp
                 context.Response.Redirect(defaultFaviconPath, true);
                 return Task.CompletedTask;
             });
-        }
-
-        private void ConfigureSerilog()
-        {
-            var logConfig = new LoggerConfiguration()
-                .ReadFrom
-                .Configuration(Configuration);
-
-            var esLogConfig = new ElasticSearchLogConfigurator(Configuration);
-            esLogConfig.Configure(logConfig);
-
-            Log.Logger = logConfig.CreateLogger();
-            StartupLogger = Log.Logger;
-
-            StartupLogger.Debug("Completed logging configuration...");
         }
     }
 }
