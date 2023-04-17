d8e22755a68d1289136551bec56d62225d32b98c
Stu Wilkes <h.s.j.wilkes@gmail.com>
2023-04-17 15:40:03 +0100

    fix(General): rm Toggle for Semantic Inline Alert

--- test/StockportWebappTests/Unit/Parsers/AlertsInlineTagParserTest.cs
+++ test/StockportWebappTests/Unit/Parsers/AlertsInlineTagParserTest.cs
@@ -17,26 +17,10 @@ namespace StockportWebappTests_Unit.Unit.Parsers
 
         public AlertsInlineTagParserTest()
         {
-            _featureToggles = new FeatureToggles() { SemanticInlineAlert = true };
+            _featureToggles = new FeatureToggles();
             _viewRenderer = new Mock<IViewRender>();
             _mockLogger = new Mock<ILogger<Alert>>();
             _alertsInlineTagParser = new AlertsInlineTagParser(_viewRenderer.Object, _mockLogger.Object, _featureToggles);
-        }
-
-        [Fact]
-        public void ShouldReplaceAlertsInlineTagWithAlertsInlineView()
-        {
-            var content = "this is some test {{Alerts-Inline:some-title}}";
-            var alertsInline = new Alert("some-title", "some-image-url", "some-subheading", "some-severity", System.DateTime.MinValue, System.DateTime.MaxValue, string.Empty, false);
-            var alertsInlineList = new List<Alert>() { alertsInline };
-            var renderResult = "RENDERED ARTICLE CONTENT";
-
-            _viewRenderer.Setup(o => o.Render("Semantic/AlertsInline", alertsInline)).Returns(renderResult);
-
-            var parsedHtml = _alertsInlineTagParser.Parse(content, alertsInlineList);
-
-            _viewRenderer.Verify(o => o.Render("Semantic/AlertsInline", alertsInline), Times.Once);
-            parsedHtml.Should().Contain(renderResult);
         }
 
         [Fact]
