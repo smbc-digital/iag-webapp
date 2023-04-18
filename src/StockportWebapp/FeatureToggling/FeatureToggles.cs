cfe71acee69a2cb6784f9f9e6030f507aefc3833
Stu Wilkes <h.s.j.wilkes@gmail.com>
2023-04-18 09:48:20 +0100

    fix(General): rm old Feature Toggles

--- src/StockportWebapp/FeatureToggling/FeatureToggles.cs
+++ src/StockportWebapp/FeatureToggling/FeatureToggles.cs
@@ -2,16 +2,6 @@ namespace StockportWebapp.FeatureToggling
 {
     public class FeatureToggles
     {
-        public bool GroupArchiveEmails { get; set; } = false;
-        public bool NameInHeader { get; set; } = false;
-        public bool EditGroupUploadImage { get; set; } = false;
-        public bool SiaSystem { get; set; } = false;
-        public bool SemanticLayout { get; set; } = false;
-        public bool ContactUsArea { get; set; } = false;
-        public bool SemanticInlineAlert { get; set; } = false;
-        public bool Newsroom { get; set; } = false;
-        public bool LeafletMap { get; set; } = false;
-        public bool CivicaPay { get; set; } = false;
-        public bool ReciteMeTrial { get; set; } = false;
+
     }
 }
