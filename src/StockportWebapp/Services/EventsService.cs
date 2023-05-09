cddddb2d731974cafd188372e26f45d12cf96458
Stu Wilkes <h.s.j.wilkes@gmail.com>
2023-05-09 06:49:03 +0100

    feat(NET6): Services

--- src/StockportWebapp/Services/EventsService.cs
+++ src/StockportWebapp/Services/EventsService.cs
@@ -1,43 +1,38 @@
-﻿using StockportWebapp.Models;
-using StockportWebapp.Repositories;
-using StockportWebapp.ViewModels;
+﻿namespace StockportWebapp.Services;
 
-namespace StockportWebapp.Services
+public interface IEventsService
 {
-    public interface IEventsService
+    Task<List<Event>> GetEventsByLimit(int limit);
+    Task<Event> GetLatestEventsItem();
+    Task<Event> GetLatestFeaturedEventItem();
+}
+
+public class EventsService : IEventsService
+{
+    private readonly IRepository _eventsRepository;
+
+    public EventsService(IRepository eventsRepository)
     {
-        Task<List<Event>> GetEventsByLimit(int limit);
-        Task<Event> GetLatestEventsItem();
-        Task<Event> GetLatestFeaturedEventItem();
+        _eventsRepository = eventsRepository;
     }
 
-    public class EventsService : IEventsService
+    public async Task<List<Event>> GetEventsByLimit(int limit)
     {
-        private readonly IRepository _eventsRepository;
+        var response = await _eventsRepository.GetLatest<EventCalendar>(limit);
+        return response.Content as List<Event>;
+    }
 
-        public EventsService(IRepository eventsRepository)
-        {
-            _eventsRepository = eventsRepository;
-        }
+    public async Task<Event> GetLatestEventsItem()
+    {
+        var response = await _eventsRepository.GetLatest<EventCalendar>(1);
+        var newsItems = response.Content as EventCalendar;
+        return newsItems?.Events?.First();
+    }
 
-        public async Task<List<Event>> GetEventsByLimit(int limit)
-        {
-            var response = await _eventsRepository.GetLatest<EventCalendar>(limit);
-            return response.Content as List<Event>;
-        }
-
-        public async Task<Event> GetLatestEventsItem()
-        {
-            var response = await _eventsRepository.GetLatest<EventCalendar>(1);
-            var newsItems = response.Content as EventCalendar;
-            return newsItems?.Events?.First();
-        }
-
-        public async Task<Event> GetLatestFeaturedEventItem()
-        {
-            var response = await _eventsRepository.GetLatestOrderByFeatured<EventCalendar>(1);
-            var newsItems = response.Content as EventCalendar;
-            return newsItems?.Events?.First();
-        }
+    public async Task<Event> GetLatestFeaturedEventItem()
+    {
+        var response = await _eventsRepository.GetLatestOrderByFeatured<EventCalendar>(1);
+        var newsItems = response.Content as EventCalendar;
+        return newsItems?.Events?.First();
     }
 }
--- src/StockportWebapp/Services/StockportApiEventsService.cs
+++ src/StockportWebapp/Services/StockportApiEventsService.cs
@@ -1,52 +1,45 @@
-﻿using StockportWebapp.ContentFactory;
-using StockportWebapp.Models;
-using StockportWebapp.ProcessedModels;
-using StockportWebapp.Repositories;
-using StockportWebapp.Utils;
+﻿namespace StockportWebapp.Services;
 
-namespace StockportWebapp.Services
+public interface IStockportApiEventsService
 {
-    public interface IStockportApiEventsService
+    Task<List<EventCategory>> GetEventCategories();
+    Task<List<Event>> GetEventsByCategory(string category, bool onlyNextOccurrence = true);
+    Task<ProcessedEvents> GetProcessedEvent(string slug, DateTime? date);
+}
+
+public class StockportApiEventsService : IStockportApiEventsService
+{
+    readonly IStockportApiRepository _stockportApiRepository;
+    readonly IUrlGeneratorSimple _urlGeneratorSimple;
+    private readonly IEventFactory _eventFactory;
+
+    public StockportApiEventsService(IStockportApiRepository stockportApiRepository, IUrlGeneratorSimple urlGeneratorSimple, IEventFactory eventFactory)
     {
-        Task<List<EventCategory>> GetEventCategories();
-        Task<List<Event>> GetEventsByCategory(string category, bool onlyNextOccurrence = true);
-        Task<ProcessedEvents> GetProcessedEvent(string slug, DateTime? date);
+        _stockportApiRepository = stockportApiRepository;
+        _urlGeneratorSimple = urlGeneratorSimple;
+        _eventFactory = eventFactory;
     }
 
-    public class StockportApiEventsService : IStockportApiEventsService
+    public async Task<List<Event>> GetEventsByCategory(string category, bool onlyNextOccurrence = true)
     {
-        readonly IStockportApiRepository _stockportApiRepository;
-        readonly IUrlGeneratorSimple _urlGeneratorSimple;
-        private readonly IEventFactory _eventFactory;
+        return await _stockportApiRepository.GetResponse<List<Event>>("by-category", new List<Query> { new Query("category", category), new Query("onlyNextOccurrence", onlyNextOccurrence.ToString()) });
+    }
 
-        public StockportApiEventsService(IStockportApiRepository stockportApiRepository, IUrlGeneratorSimple urlGeneratorSimple, IEventFactory eventFactory)
-        {
-            _stockportApiRepository = stockportApiRepository;
-            _urlGeneratorSimple = urlGeneratorSimple;
-            _eventFactory = eventFactory;
-        }
+    public async Task<List<EventCategory>> GetEventCategories()
+    {
+        return await _stockportApiRepository.GetResponse<List<EventCategory>>();
+    }
 
-        public async Task<List<Event>> GetEventsByCategory(string category, bool onlyNextOccurrence = true)
-        {
-            return await _stockportApiRepository.GetResponse<List<Event>>("by-category", new List<Query> { new Query("category", category), new Query("onlyNextOccurrence", onlyNextOccurrence.ToString()) });
-        }
+    public async Task<ProcessedEvents> GetProcessedEvent(string slug, DateTime? date)
+    {
+        var queries = new List<Query>();
+        if (date.HasValue) queries.Add(new Query("date", date.Value.ToString("yyyy-MM-dd")));
 
-        public async Task<List<EventCategory>> GetEventCategories()
-        {
-            return await _stockportApiRepository.GetResponse<List<EventCategory>>();
-        }
+        var eventItem = await _stockportApiRepository.GetResponse<Event>(slug, queries);
 
-        public async Task<ProcessedEvents> GetProcessedEvent(string slug, DateTime? date)
-        {
-            var queries = new List<Query>();
-            if (date.HasValue) queries.Add(new Query("date", date.Value.ToString("yyyy-MM-dd")));
+        if (eventItem == null) return null;
 
-            var eventItem = await _stockportApiRepository.GetResponse<Event>(slug, queries);
-
-            if (eventItem == null) return null;
-
-            var processedEvent = _eventFactory.Build(eventItem);
-            return processedEvent;
-        }
+        var processedEvent = _eventFactory.Build(eventItem);
+        return processedEvent;
     }
 }
