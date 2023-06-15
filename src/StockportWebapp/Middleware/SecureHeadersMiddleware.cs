7a854c4f884d4135bfd3628d6508ed56750da991
Elena Secara <elena.secara@stockport.gov.uk>
2023-06-15 15:08:39 +0100

    chore(SecureHeadersMiddleware): update custom config for security headers

--- src/StockportWebapp/Middleware/SecureHeadersMiddleware.cs
+++ src/StockportWebapp/Middleware/SecureHeadersMiddleware.cs
@@ -8,7 +8,16 @@ public class SecureHeadersMiddleware
     public static SecureHeadersMiddlewareConfiguration CustomConfiguration() =>
         SecureHeadersMiddlewareBuilder
             .CreateBuilder()
+            .UseHsts()
+            .UseXFrameOptions()
             .UseXssProtection()
+            .UseContentTypeOptions()
+            .UseContentSecurityPolicy()
+            .UseContentDefaultSecurityPolicy()
+            .UseReferrerPolicy()
             .UseCacheControl()
+            .RemovePoweredByHeader()
+            .UseCrossOriginResourcePolicy()
+            .UsePermittedCrossDomainPolicies()
             .Build();
 }
