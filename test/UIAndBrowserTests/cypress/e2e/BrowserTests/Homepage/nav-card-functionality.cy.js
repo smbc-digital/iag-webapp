0de5236b752d50f7d373104696e92b19c4da81da
Yasmin Tubbritt <yasmin.tubbritt@stockport.gov.uk>
2023-06-26 14:49:43 +0100

    Navard cypress test

--- test/UIAndBrowserTests/cypress/e2e/BrowserTests/Homepage/nav-card-functionality.cy.js
+++ test/UIAndBrowserTests/cypress/e2e/BrowserTests/Homepage/nav-card-functionality.cy.js
@@ -1,16 +1,19 @@
 describe("Navcard functionality", () => {
   const viewports = ["iphone-x", [1440, 900]];
   viewports.map((size) => {
-    it(`tests on ${size} screen to check the Navcards are clickable and go to a new page location`, () => {
+    it(`tests on ${size} screen to check a Navcard can be clicked and loads new page`, () => {
       if (Cypress._.isArray(size)) {
         cy.viewport(size[0], size[1]);
       } else {
         cy.viewport(size);
       }
       cy.visit("")
+      //Content for the Navcards is content managed, only testing the componant is clickable. We can't control errors caused by CMS.
       cy.get('[data-cy="nav-card-link"]')
       .should("have.length", 9)
-      .click({ multiple: true });
+      .first()
+      .click()
+      cy.url().should('not.eq','https://qa-iag-stockportgov.smbcdigital.net/')
     });
   });
 });
