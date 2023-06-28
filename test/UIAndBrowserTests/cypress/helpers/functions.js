3f9ef7d04dcac3ecab039d4e39efae7893ecfcdc
Harry Woodall <harry.woodall@stockport.gov.uk>
2023-06-28 11:20:16 +0100

    chore(UI tests): add more services button test

--- test/UIAndBrowserTests/cypress/helpers/functions.js
+++ test/UIAndBrowserTests/cypress/helpers/functions.js
@@ -4,3 +4,16 @@ export const closeAlerts = () => {
         .click({multiple: true})
         .should('not.be.visible')
 }
+
+export const hexToRgb = (hex) => {
+    console.log("HEX: ", hex);
+    var parsedValues = /^#?([a-f\d]{1,2})([a-f\d]{1,2})([a-f\d]{1,2})$/i.exec(hex);
+
+    if (parsedValues) {
+        const r = parseInt((parsedValues[1] + parsedValues[1]).slice(-2), 16);
+        const g = parseInt((parsedValues[2] + parsedValues[2]).slice(-2), 16);
+        const b = parseInt((parsedValues[3] + parsedValues[3]).slice(-2), 16);
+        return `rgb(${r}, ${g}, ${b})`;
+    }
+    return null;
+}
