a01110489497e190db2070db23bb3c4f2ed26d8c
Jonathan Hadley <jonathan.hadley@stockport.gov.uk>
2023-07-05 11:59:56 +0100

    patch: fix broken tests and prepare for processed homepage tests

--- src/StockportWebapp/Models/Alert.cs
+++ src/StockportWebapp/Models/Alert.cs
@@ -12,7 +12,13 @@ public class Alert
     public bool IsStatic { get; }
     public string ImageUrl { get; }
 
-    public Alert(string title, string subHeading, string body, string severity, DateTime sunriseDate, DateTime sunsetDate, string slug, bool isStatic, string imageUrl)
+    public Alert(string title, string subHeading, string body, string severity, DateTime sunriseDate, DateTime sunsetDate, string slug, bool isStatic, string imageUrl) 
+        : this(title, subHeading, body, severity, sunriseDate, sunsetDate, slug, isStatic)
+    {
+        ImageUrl = imageUrl;
+    }
+
+    public Alert(string title, string subHeading, string body, string severity, DateTime sunriseDate, DateTime sunsetDate, string slug, bool isStatic)
     {
         Title = title;
         SubHeading = subHeading;
@@ -22,7 +28,7 @@ public class Alert
         SunsetDate = sunsetDate;
         Slug = slug;
         IsStatic = isStatic;
-        ImageUrl = imageUrl;
+        ImageUrl = string.Empty;
     }
 }
 
---
+++ test/StockportWebappTests/Unit/Model/ProcessedAlertTests.cs
@@ -0,0 +1,12 @@
+﻿namespace StockportWebappTests_Unit.Unit.Model;
+
+public class ProcessedHomepageTests
+{
+    private readonly ProcessedHomepage processedHomepage = new ProcessedHomepage(Enumerable.Empty<string>(), "Featured Tasks", "Featured tasks summary", Enumerable.Empty<SubItem>(), Enumerable.Empty<SubItem>(), Enumerable.Empty<Alert>(), Enumerable.Empty<CarouselContent>(), string.Empty, Enumerable.Empty<News>(), "Free text", null, "Event category", "meta description", null);
+
+    [Fact (Skip = "Not Implemented")]
+    public void Should_Seperate_CondolenceAlerts_From_Other_Alerts()
+    {
+        
+    }
+}
