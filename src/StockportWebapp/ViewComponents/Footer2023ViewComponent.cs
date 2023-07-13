1f2b803d41419a9f3ed8ea0c1d559fce499c152d
Jonathan Hadley <jonathan.hadley@stockport.gov.uk>
2023-07-13 16:13:11 +0100

    minor: updates footer and removes logos, atoz and changes Email subscription #9056

---
+++ src/StockportWebapp/ViewComponents/Footer2023ViewComponent.cs
@@ -0,0 +1,27 @@
+﻿namespace StockportWebapp.ViewComponents;
+
+public class Footer2023ViewComponent : ViewComponent
+{
+    private readonly IRepository _repository;
+    private readonly ILogger<FooterViewComponent> _logger;
+
+    public Footer2023ViewComponent(IRepository repository, ILogger<FooterViewComponent> logger)
+    {
+        _repository = repository;
+        _logger = logger;
+    }
+
+    public async Task<IViewComponentResult> InvokeAsync()
+    {
+        _logger.LogInformation("Call to retrieve the footer");
+
+        var footerHttpResponse = await _repository.Get<Footer>();
+
+        if (!footerHttpResponse.IsSuccessful())
+            return await Task.FromResult(View("NoFooterFound"));
+
+        var model = footerHttpResponse.Content as Footer;
+
+        return await Task.FromResult(View(model));
+    }
+}
