b0d9d84ae1857d884ab338892683dd7d16b3985b
Elena Secara <elena.secara@stockport.gov.uk>
2023-07-18 15:56:50 +0100

    feat(FeatureCard): add featureCard partial + styling

---
+++ src/StockportWebapp/ViewModels/FeatureCardViewModel.cs
@@ -0,0 +1,10 @@
+namespace StockportWebapp.ViewModels;
+
+public class FeatureCardViewModel
+{
+    public string Title { get; set; }
+    public string Teaser { get; set; }
+    public DateTime Date { get; set; }
+    public string StartTime { get; set; }
+
+}
