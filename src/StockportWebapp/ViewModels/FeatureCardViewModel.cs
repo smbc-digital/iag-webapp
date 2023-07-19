b4ef4707374bc769772bf23a7ae5651caecd0cc7
Harry Woodall <harry.woodall@stockport.gov.uk>
2023-07-19 12:27:38 +0100

    style(whats happening): add styling

--- src/StockportWebapp/ViewModels/FeatureCardViewModel.cs
+++ src/StockportWebapp/ViewModels/FeatureCardViewModel.cs
@@ -4,7 +4,9 @@ public class FeatureCardViewModel
 {
     public string Title { get; set; }
     public string Teaser { get; set; }
+    public string ButtonText { get; set; }
     public DateTime Date { get; set; }
     public string StartTime { get; set; }
+    public string Image { get; set; }
 
 }
