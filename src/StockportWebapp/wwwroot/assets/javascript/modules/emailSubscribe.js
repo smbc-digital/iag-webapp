08816c09792f7d7634912ee21ecda1f7ca5f0371
Elena Secara <elena.secara@stockport.gov.uk>
2023-08-11 14:36:03 +0100

    fix(Footer/EmailSubscribe): fix custom error messages + links open in the same tab

---
+++ src/StockportWebapp/wwwroot/assets/javascript/modules/emailSubscribe.js
@@ -0,0 +1,20 @@
+﻿define(['jquery'], function ($) {
+    return {
+        Init: function () {
+            const emailInput = document.getElementsByName('emailAddress')[0];
+  
+            emailInput.addEventListener('input', handler, false);
+            emailInput.addEventListener('invalid', handler, false);
+        
+            function handler(event) {
+                if (emailInput.validity.valueMissing) {
+                    emailInput.setCustomValidity('Enter an email address');
+                } else if (emailInput.validity.typeMismatch) {
+                    emailInput.setCustomValidity('Enter a valid email address');
+                } else {
+                    emailInput.setCustomValidity('');
+                }
+            }
+        }
+    };
+});
