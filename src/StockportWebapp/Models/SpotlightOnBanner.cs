4ae4972094e6f359d4f247b5f6a75dfc27211202
Elena Secara <elena.secara@stockport.gov.uk>
2023-08-15 09:14:35 +0100

    feat(spotlight on): add spotlight component

---
+++ src/StockportWebapp/Models/SpotlightOnBanner.cs
@@ -0,0 +1,25 @@
+﻿namespace StockportWebapp.Models;
+
+public class SpotlightOnBanner
+{
+    public string Title { get; }
+    public string Image { get; }
+    public string AltText { get; }
+    public string Teaser { get; }
+    public string Link { get; }
+
+    public SpotlightOnBanner(string title, string image, string altText, string teaser, string link)
+    {
+        Title = title;
+        Image = image;
+        AltText = altText;
+        Teaser = teaser;
+        Link = link;
+    }
+}
+
+
+public class NullSpotlightOnBanner : SpotlightOnBanner
+{
+    public NullSpotlightOnBanner() : base(string.Empty, string.Empty, string.Empty, string.Empty, string.Empty) { }
+}
