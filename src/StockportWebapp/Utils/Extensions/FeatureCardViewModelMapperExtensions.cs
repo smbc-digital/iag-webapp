1e2a37b11aec62c1e9a57e5b8c73a773d7327acd
Jonathan Hadley <jonathan.hadley@stockport.gov.uk>
2023-09-08 10:42:47 +0100

    minor: refactor of FeatureCard and homepage to remove mapping login from a view and changing the way some async calls are made

---
+++ src/StockportWebapp/Utils/Extensions/FeatureCardViewModelMapperExtensions.cs
@@ -0,0 +1,34 @@
+﻿namespace StockportWebapp.Utils.Extensions
+{
+    public static class FeatureCardViewModelMapperExtensions
+    {
+        public static FeatureCardViewModel MapToFeatureCard(this Event featuredEvent) => 
+            new FeatureCardViewModel
+            {
+                Title = featuredEvent.Title,
+                Teaser = featuredEvent.Teaser,
+                Date = featuredEvent.EventDate,
+                StartTime = featuredEvent.StartTime,
+                Image = featuredEvent.ImageUrl,
+                ButtonText = "View more events",
+                Slug = String.Concat("/events/", featuredEvent.Slug),
+                ButtonUrl = "Events",
+                HeaderText = "Upcoming event"
+            };
+
+        public static FeatureCardViewModel MapToFeatureCard(this News featuredNews) =>
+            new FeatureCardViewModel
+            {
+                Title = featuredNews.Title, 
+                Teaser = featuredNews.Teaser, 
+                Date = featuredNews.UpdatedAt, 
+                StartTime = string.Empty, 
+                Image = featuredNews.Image, 
+                ButtonText = "View more news",
+                Slug = String.Concat("/news/", featuredNews.Slug),
+                ButtonUrl = "Comms",
+                HeaderText = "Latest news"
+            };
+    }
+}
+
