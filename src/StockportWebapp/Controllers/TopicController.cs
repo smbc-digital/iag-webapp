48bd7cf4069604a2828445b16377f3cc2d084532
Harry Woodall <harry.woodall@stockport.gov.uk>
2023-09-20 10:49:29 +0100

    feat(topic redesign): add template file

--- src/StockportWebapp/Controllers/TopicController.cs
+++ src/StockportWebapp/Controllers/TopicController.cs
@@ -7,13 +7,15 @@ public class TopicController : Controller
     private readonly BusinessId _businessId;
     private readonly ITopicRepository _topicRepository;
     private readonly IStockportApiEventsService _stockportApiEventsService;
+    private readonly IFeatureManager _featureManager;
 
-    public TopicController(ITopicRepository repository, IApplicationConfiguration config, BusinessId businessId, IStockportApiEventsService stockportApiService)
+    public TopicController(ITopicRepository repository, IApplicationConfiguration config, BusinessId businessId, IStockportApiEventsService stockportApiService, IFeatureManager featureManager)
     {
         _config = config;
         _businessId = businessId;
         _topicRepository = repository;
         _stockportApiEventsService = stockportApiService;
+        _featureManager = featureManager;
     }
 
     [Route("/topic/{topicSlug}")]
@@ -32,6 +34,9 @@ public class TopicController : Controller
 
         var eventsFromApi = !string.IsNullOrEmpty(processedTopic.EventCategory) ? await _stockportApiEventsService.GetEventsByCategory(processedTopic.EventCategory) : new List<Event>();
         topicViewModel.EventsFromApi = eventsFromApi?.Take(3).ToList();
+
+        if(await _featureManager.IsEnabledAsync("SiteRedesign") && await _featureManager.IsEnabledAsync("TopicRedesign") && _businessId.ToString().Equals("stockportgov"))
+            return View("Index2023", topicViewModel);
 
         return View(topicViewModel);
     }
