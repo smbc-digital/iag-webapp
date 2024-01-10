d6e87844524d6b3476b5986d04d7bc0568661849
Jonathan Hadley <jonathan.hadley@stockport.gov.uk>
2024-01-10 13:08:19 +0000

    minor: refactor and enable wildcard slug for both results and entry

--- src/StockportWebapp/Controllers/DirectoryController.cs
+++ src/StockportWebapp/Controllers/DirectoryController.cs
@@ -12,8 +12,7 @@ public class DirectoryController : Controller
         _directoryRepository = directoryRepository;
     }
 
-    [Route("/directories/{slug}")]
-
+    [Route("/directories/{**slug}")]
     public async Task<IActionResult> Directory(string slug)
     {
         var directoryHttpResponse = await _directoryRepository.Get<Directory>(slug);
@@ -35,50 +34,28 @@ public class DirectoryController : Controller
     }
 
     [HttpGet]
-    [Route("/directories/results/{slug}")]
-    [Route("/directories/results/{parentSlug1}/{slug}")]
-    [Route("/directories/results/{parentSlug2}/{parentSlug1}/{slug}")]
-    [Route("/directories/results/{parentSlug3}/{parentSlug2}/{parentSlug1}/{slug}")]
-    [Route("/directories/results/{parentSlug4}/{parentSlug3}/{parentSlug2}/{parentSlug1}/{slug}")]
-    public async Task<IActionResult> DirectoryResults([Required][FromRoute]string slug, string parentSlug1, string parentSlug2, string parentSlug3, string parentSlug4)
+    [Route("/directories/results/{**slug}")]
+    public async Task<IActionResult> DirectoryResults([Required][FromRoute]string slug)
     {
-        var directoryHttpResponse = await _directoryRepository.Get<Directory>(slug);
-        
+        var pageLocation = ProcessWildcardSlug(slug);
+
+        var directoryHttpResponse = await _directoryRepository.Get<Directory>(pageLocation.Slug);
         if (!directoryHttpResponse.IsSuccessful())
             return directoryHttpResponse;
 
+        List<Directory> parentDirectories = await GetParentDirectories(pageLocation.ParentSlugs);
+
         var directory = directoryHttpResponse.Content as Directory;
-
-        DirectoryViewModel directoryViewModel = new()
-        {
-            Directory = directory,
-            FilteredEntries = _directoryRepository.GetFilteredEntryForDirectories(directory)
-        };
-        
-        
-        //directoryViewModel.Directory.AddToRouteValuesIfNotNullOrEmpty(processedDirectory.Slug, true);
-        //directoryViewModel.Directory.AddToRouteValuesIfNotNullOrEmpty(parentSlug1);
-        //directoryViewModel.Directory.AddToRouteValuesIfNotNullOrEmpty(parentSlug2);
-        //directoryViewModel.Directory.AddToRouteValuesIfNotNullOrEmpty(parentSlug3);
-        //directoryViewModel.Directory.AddToRouteValuesIfNotNullOrEmpty(parentSlug4);
-
-        //if (!string.IsNullOrEmpty(parentSlug1))
-        //{
-        //    var directoryLookupHttpResponse = await _directoryRepository.Get<Directory>(parentSlug1);
-
-        //    if (directoryLookupHttpResponse.IsSuccessful())
-        //    {
-        //        var lookedUpDirectory = directoryLookupHttpResponse.Content as Directory;
-        //        directoryViewModel.Breadcrumbs.Add(new Crumb(lookedUpDirectory.Title, parentSlug1, "Directories"));
-        //    }
-        //}
-        
-        
-        return View("results", directoryViewModel);
+        return View("results", new DirectoryViewModel()
+            {
+                Directory = directory,
+                FilteredEntries = _directoryRepository.GetFilteredEntryForDirectories(directory),
+                Breadcrumbs = GetBreadcrumbsForDirectories(parentDirectories)
+            });
     }
 
     [HttpPost]
-    [Route("/directories/results/{slug}")]
+    [Route("/directories/results/{**slug}")]
     public async Task<IActionResult> FilterResults(string slug)
     {
         var directoryHttpResponse = await _directoryRepository.Get<Directory>(slug);
@@ -112,52 +89,67 @@ public class DirectoryController : Controller
     [Route("directories/entry/{**slug}")]
     public async Task<IActionResult> DirectoryEntry(string slug)
     {
-        // Get slug values from catch all
-        var slugValues = slug?.Split('/') ?? Array.Empty<string>();
-
-        // Last value will always be the entry slug
-        var entrySlug = slugValues.Last();
+        var pageLocation = ProcessWildcardSlug(slug);
 
         // Get the requested entry
-        var directoryEntryHttpResponse = await _directoryRepository.GetEntry<DirectoryEntry>(entrySlug);
+        var directoryEntryHttpResponse = await _directoryRepository.GetEntry<DirectoryEntry>(pageLocation.Slug);
         if (!directoryEntryHttpResponse.IsSuccessful())
             return directoryEntryHttpResponse;
 
-        var processedEntryDirectory = directoryEntryHttpResponse.Content as DirectoryEntry;
-
-        // Anything before the (last) entry slug is directory hierarchy
-        var directorySlugs = slugValues.SkipLast(1).ToList();
-        List<Directory> parentDirectories = new();
-
         // Get the parent directories
-        HttpResponse directoryHttpResponse;
-        foreach (var directorySlug in directorySlugs)
-        {
-            directoryHttpResponse = await _directoryRepository.Get<Directory>(directorySlug);
-            if (!directoryHttpResponse.IsSuccessful())
-                return directoryEntryHttpResponse;
-
-            var p = directoryHttpResponse.Content as Directory;
-            parentDirectories.Add(p);
-        }
-
-        // Create breadcrumbs for the parent
-        List<Crumb> breadcrumbs = new();
-        for (int i = 0; i < parentDirectories.Count; i++)
-        {
-            var directory = parentDirectories[i];
-            var relativeParentDirectories = parentDirectories.GetRange(0, i);
-            var directorySlug = string.Join('/', relativeParentDirectories);
-            breadcrumbs.Add(new(directory.Title, directorySlug, "Directories"));
-        }
+        List<Directory> parentDirectories = await GetParentDirectories(pageLocation.ParentSlugs);
 
         DirectoryViewModel directoryViewModel = new()
         {
             Directory = parentDirectories.First(),
-            DirectoryEntry = processedEntryDirectory,
-            Breadcrumbs = breadcrumbs
-        };
+            DirectoryEntry = directoryEntryHttpResponse.Content as DirectoryEntry,
+            Breadcrumbs = GetBreadcrumbsForDirectories(parentDirectories)
+    };
 
         return View(directoryViewModel);
+    }
+
+    private PageLocation ProcessWildcardSlug(string slug)
+    {
+        var slugValues = slug?.Split('/') ?? Array.Empty<string>();
+        return new PageLocation(slugValues.Last(), slugValues.SkipLast(1).ToList());
+    }
+
+    private List<Crumb> GetBreadcrumbsForDirectories(IList<Directory> parentDirectories) 
+    {
+        List<Crumb> breadcrumbs = new();
+        for (int i = 0; i < parentDirectories.Count; i++)
+        {
+            var directory = parentDirectories[i];
+            var relativeUrl = string.Join("/", parentDirectories.Take(i + 1).Select(_ => _.Slug));
+            breadcrumbs.Add(new Crumb(directory.Title, $"directories/{relativeUrl}", "Directories"));
+        }
+
+        return breadcrumbs;
+    }
+
+    private async Task<List<Directory>> GetParentDirectories(IEnumerable<string> parentSlugs)
+    {
+        List<Directory> parentDirectories = new();
+        HttpResponse directoryHttpResponse;
+        foreach (var directorySlug in parentSlugs)
+        {
+            directoryHttpResponse = await _directoryRepository.Get<Directory>(directorySlug);
+            if (directoryHttpResponse.IsSuccessful())
+                parentDirectories.Add(directoryHttpResponse.Content as Directory);
+        }
+        return parentDirectories;
+    }
+}
+
+public class PageLocation
+{
+    public string Slug { get; private set; }
+    public IEnumerable<string> ParentSlugs { get; private set; }
+
+    public PageLocation(string slug, IEnumerable<string> parentSlugs)
+    {
+        Slug = slug;
+        ParentSlugs = parentSlugs;
     }
 }
