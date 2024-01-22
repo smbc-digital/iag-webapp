fa545baed8c6718d95f524b1b419d02dc6046cd5
Jonathan Hadley <jonathan.hadley@stockport.gov.uk>
2024-01-22 10:42:31 +0000

    minor: buttons to show/hide map and twaeks to the way the map displays

--- src/StockportWebapp/Extensions/DirectoryExtensions.cs
+++ src/StockportWebapp/Extensions/DirectoryExtensions.cs
@@ -13,6 +13,7 @@ public static class DirectoryExtensions
         var kml = new Kml();
 
         // Map sttyling - https://github.com/samcragg/sharpkml/blob/main/Examples/CreateIconStyle.cs
+        // https://www.google.com/maps/d/viewer?mid=1icXjgXJ5da1l2BQjMNgXAI4dlkw&hl=en_US&ll=-0.0047047627978486686%2C0.006783479799636893&z=17
         var style = new Style();
         style.Id = "RedDot";
         style.Icon = new IconStyle();   
