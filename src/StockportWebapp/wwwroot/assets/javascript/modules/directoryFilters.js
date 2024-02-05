7905fd150a11c8674c0f63a9be93c7d407dd512a
michael hayes <michael.hayes@stockport.gov.uk>
2024-02-05 10:44:14 +0000

    fix(results): change expanded to closed for tags

--- src/StockportWebapp/wwwroot/assets/javascript/modules/directoryFilters.js
+++ src/StockportWebapp/wwwroot/assets/javascript/modules/directoryFilters.js
@@ -3,7 +3,7 @@ define(["jquery"], function ($) {
     var init = function () {
         $(document).ready(function () {
             $('.directory-results__collapsible').on('click', function() {
-                $(this).find("span").toggleClass("expanded");
+                $(this).find("span").toggleClass("closed");
                 $(this).next(".directory-filters__content").toggle();
             });
     })}
