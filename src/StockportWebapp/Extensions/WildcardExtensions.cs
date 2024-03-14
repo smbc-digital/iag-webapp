3d750a26b4b255b8912a9d65a3e7993b4727e9a4
Jonathan Hadley <jonathan.hadley@stockport.gov.uk>
2024-03-14 16:51:58 +0000

    patch: fix double slug causes 404 issue for wildcard slugs and add tests

--- src/StockportWebapp/Extensions/WildcardExtensions.cs
+++ src/StockportWebapp/Extensions/WildcardExtensions.cs
@@ -3,8 +3,12 @@ public static class WildcardExtensions
 {
     public static PageLocation ProcessSlug(string slug)
     {
+        if (string.IsNullOrEmpty(slug))
+            return new PageLocation(string.Empty, new List<string>()) ;
+
         slug = slug.TrimEnd('/');
         var slugValues = slug?.Split('/') ?? Array.Empty<string>();
-        return new PageLocation(slugValues.Last(), slugValues.SkipLast(1).ToList());
+        slugValues = slugValues.Select(slug => slug.Trim(new char[] { '/', '\\' })).ToArray();
+        return new PageLocation(slugValues.Last(), slugValues.Where(slug => !string.IsNullOrEmpty(slug)).SkipLast(1).ToList());
     }
 }
