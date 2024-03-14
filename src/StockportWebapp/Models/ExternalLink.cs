5915de0b76ddf9e321cfad77269c34c90647d524
michael hayes <michael.hayes@stockport.gov.uk>
2024-03-14 09:42:47 +0000

    feat(externalLinks): add external links, related content to directory

---
+++ src/StockportWebapp/Models/ExternalLink.cs
@@ -0,0 +1,9 @@
+namespace StockportWebapp.Models
+{
+    public class ExternalLink
+    {
+        public string Title { get; set; } = string.Empty;
+        public string URL { get; set; } = string.Empty;
+        public string Teaser { get; set; } = string.Empty;
+    }
+}
