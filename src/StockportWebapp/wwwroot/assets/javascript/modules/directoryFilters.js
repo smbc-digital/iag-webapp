85b9ba86bdee14469e26a9687996108a48c7824b
Elena Secara <elena.secara@stockport.gov.uk>
2024-04-17 08:32:48 +0100

    chore(directoryFilters): update script to hide theme groups when js is enabled

--- src/StockportWebapp/wwwroot/assets/javascript/modules/directoryFilters.js
+++ src/StockportWebapp/wwwroot/assets/javascript/modules/directoryFilters.js
@@ -8,6 +8,14 @@ define(["jquery"], function ($) {
                 $(this).attr('aria-expanded', $(this).attr('aria-expanded') === 'true' ? false : true);
             });
 
+            $('.directory-filters__content').each(function(index, themeGroup) {
+                if (index !== 0 && !$(themeGroup).find('.directory-filters__checkbox:checked').length) {
+                    $(themeGroup).hide();
+                    const link = $(themeGroup).prev('.directory-results__collapsible');
+                    link.attr('aria-expanded', 'false');
+                }
+            });
+
             $('#order-by').change(function() {
                 $(this).closest('form').submit();
             });
