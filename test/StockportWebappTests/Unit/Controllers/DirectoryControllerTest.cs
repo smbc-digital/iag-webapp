5d2044ce40bc566fd48a5608003d4bdab9ab10f3
Elena Secara <elena.secara@stockport.gov.uk>
2024-04-18 15:27:10 +0100

    fix(DirectoryController): fix failing test

--- test/StockportWebappTests/Unit/Controllers/DirectoryControllerTest.cs
+++ test/StockportWebappTests/Unit/Controllers/DirectoryControllerTest.cs
@@ -270,6 +270,6 @@ public class DirectoryControllerTest
         var result = await _directoryController.DirectoryResults("slug", Array.Empty<string>(), string.Empty, "search me", 0);
 
         // Assert
-        _directoryService.Verify(service => service.GetSearchedEntryForDirectories(It.IsAny<IEnumerable<DirectoryEntry>>(), It.IsAny<string>()), Times.Exactly(2));
+        _directoryService.Verify(service => service.GetSearchedEntryForDirectories(It.IsAny<IEnumerable<DirectoryEntry>>(), It.IsAny<string>()), Times.Exactly(1));
     }
 }
