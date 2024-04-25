84a5ed5b28068c9b10cb1c66f19e0b0406de0665
Jonathan Hadley <jonathan.hadley@stockport.gov.uk>
2024-04-25 13:26:52 +0100

    patch: back out some refactor to DirectoryEntry and ViewModel changes

--- src/StockportWebapp/ViewModels/DirectoryEntryViewModel.cs
+++ src/StockportWebapp/ViewModels/DirectoryEntryViewModel.cs
@@ -11,20 +11,7 @@ public class DirectoryEntryViewModel : ISlugComparable
     public DirectoryEntryViewModel(string slug, DirectoryEntry directoryEntry)
     {
         Slug = slug;
-        Name = directoryEntry.Name;
-        MetaDescription = directoryEntry.MetaDescription;
-        Description = directoryEntry.Description;
-        PhoneNumber = directoryEntry.PhoneNumber;
-        Email = directoryEntry.Email;
-        Website = directoryEntry.Website;
-        Twitter = directoryEntry.Twitter;
-        Facebook = directoryEntry.Facebook;
-        Youtube = directoryEntry.Youtube;
-        Instagram = directoryEntry.Instagram;
-        LinkedIn = directoryEntry.LinkedIn;
-        Address = directoryEntry.Address;
-        Image = directoryEntry.Image;
-        Branding = directoryEntry.Branding;
+        DirectoryEntry = directoryEntry;
     }
     public DirectoryEntryViewModel(string slug, DirectoryEntry directoryEntry, IEnumerable<Crumb> breadcrumbs) 
         : this(slug, directoryEntry)
@@ -39,36 +26,21 @@ public class DirectoryEntryViewModel : ISlugComparable
         => IsPinned = isPinned;    
 
     // Core page details
+
+    public DirectoryEntry DirectoryEntry { get; set; }
     public string Slug { get; set; }
-    public string Name { get; set; }
-    public string Provider { get; set; }
-    public string Description { get; set; }
-    public string Teaser { get; set; }
-    public string Image { get; set; }
-    public string MetaDescription { get; set; }
     public IEnumerable<Crumb> Breadcrumbs { get; set; }
-    public string PhoneNumber { get; set; }
-    public string Email { get; set; }
-    public string Website { get; set; }
-    public string Twitter { get; set; }
-    public string Facebook { get; set; }
-    public string Youtube { get; set; }
-    public string Instagram { get; set; }
-    public string LinkedIn { get; set; }
-    public string Address { get; set; }
     public bool IsPinned { get; set; }
-    public List<GroupBranding> Branding { get; set; }
-    public IEnumerable<FilterTheme> Themes { get; set; } = new List<FilterTheme>();
-    public IEnumerable<Filter> HighlightedFilters => Themes?
+    public IEnumerable<Filter> HighlightedFilters => DirectoryEntry.Themes?
                                                         .SelectMany(_ => _.Filters.Where(_ => _.Highlight.Equals(true)))
                                                         .ToList();
 
-    public bool DisplaySocials => !string.IsNullOrEmpty(Facebook) 
-                                    || !string.IsNullOrEmpty(Twitter)
-                                    || !string.IsNullOrEmpty(Youtube)
-                                    || !string.IsNullOrEmpty(Instagram)
-                                    || !string.IsNullOrEmpty(LinkedIn);
+    public bool DisplaySocials => !string.IsNullOrEmpty(DirectoryEntry.Facebook) 
+                                    || !string.IsNullOrEmpty(DirectoryEntry.Twitter)
+                                    || !string.IsNullOrEmpty(DirectoryEntry.Youtube)
+                                    || !string.IsNullOrEmpty(DirectoryEntry.Instagram)
+                                    || !string.IsNullOrEmpty(DirectoryEntry.LinkedIn);
     public bool HasPrimaryContact 
-        => !string.IsNullOrEmpty(PhoneNumber) ||  !string.IsNullOrEmpty(Email);
-    public bool DisplayContactUs => !string.IsNullOrEmpty(Website) || HasPrimaryContact || DisplaySocials;
+        => !string.IsNullOrEmpty(DirectoryEntry.PhoneNumber) ||  !string.IsNullOrEmpty(DirectoryEntry.Email);
+    public bool DisplayContactUs => !string.IsNullOrEmpty(DirectoryEntry.Website) || HasPrimaryContact || DisplaySocials;
 }
