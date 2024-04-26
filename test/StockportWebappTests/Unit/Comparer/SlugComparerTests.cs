2ec01867cfe8e1e78e083795c0d86cb3c256275a
Jonathan Hadley <jonathan.hadley@stockport.gov.uk>
2024-04-26 11:59:48 +0100

    patch: add test for comparer

--- test/StockportWebappTests/Unit/Comparer/SlugComparerTests.cs
+++ test/StockportWebappTests/Unit/Comparer/SlugComparerTests.cs
@@ -5,18 +5,35 @@ namespace StockportWebappTests_Unit.Unit.Comparers;
 public class SlugComparerTests
 {
     [Fact]
-    public void Slug_ReturnCorrectlyFormedSlug()
+    public void SlugComparer_Equals_Returns_True_ForSameObject()
     {
-        var viewModel = new DirectorySearchResultViewModel
-        {
-            Entry = new DirectoryEntryViewModel { Slug = "DirectoryEntry" },
-            DirectorySlug = "Directory"
-        };
-        
         SlugComparer comparer = new SlugComparer();
-        
-        var result = viewModel.Slug;
+        DirectoryEntry entry = new DirectoryEntry() { Slug="TestSlug" };
+        var result = comparer.Equals(entry, entry);
 
-        Assert.Equal("Directory/DirectoryEntry", result);
+        Assert.True(result);
+    }
+
+        [Fact]
+    public void SlugComparer_Equals_Returns_False_ForDifferentObject()
+    {
+        SlugComparer comparer = new SlugComparer();
+        DirectoryEntry entry = new DirectoryEntry() { Slug="TestSlug" };
+        DirectoryEntry entry2 = new DirectoryEntry() { Slug="TestSlug2" };
+        var result = comparer.Equals(entry, entry2);
+
+        Assert.False(result);
+    }
+
+    [Fact]
+    [InlineData]
+    public void SlugComparer_Equals_Returns_False_ForDifferentObject()
+    {
+        SlugComparer comparer = new SlugComparer();
+        DirectoryEntry entry = new DirectoryEntry() { Slug="TestSlug" };
+        DirectoryEntry entry2 = new DirectoryEntry() { Slug="TestSlug2" };
+        var result = comparer.Equals(entry, entry2);
+
+        Assert.False(result);
     }
 }
