74b4335865e625d91e019bb9da12806c80935325
Elena Secara <elena.secara@stockport.gov.uk>
2024-07-23 16:52:47 +0100

    test(PrivacyNoticeControllerTest): add missing tests

---
+++ test/StockportWebappTests/Unit/Controllers/PrivacyNoticeControllerTest.cs
@@ -0,0 +1,48 @@
+﻿using Xunit.Sdk;
+
+namespace StockportWebappTests_Unit.Unit.Controllers;
+
+public class PrivacyNoticeControllerTest
+{
+    private readonly PrivacyNoticeController _privacyNoticeController;
+    private readonly Mock<IProcessedContentRepository> _processedRepository = new();
+
+
+    public PrivacyNoticeControllerTest() => _privacyNoticeController = new(_processedRepository.Object);
+
+    [Fact]
+    public async Task Detail_ReturnsPrivacyNoticeView()
+    {
+        // Arrange
+        ProcessedPrivacyNotice privacyNotice = new("slug", "title", "category", "purpose", "type of data",
+            "legislation", "obtained", "externally shared", "retention period",
+            false, false, "urlOne", "urlTwo", "urlThree", new List<Crumb>(), new NullTopic()
+        );
+
+        _processedRepository
+            .Setup(_ => _.Get<PrivacyNotice>(It.IsAny<string>(), It.IsAny<List<Query>>()))
+            .ReturnsAsync(HttpResponse.Successful(200, privacyNotice));
+
+        // Act
+        ViewResult view = await _privacyNoticeController.Detail("slug") as ViewResult;
+        PrivacyNoticeViewModel viewModel = view.ViewData.Model as PrivacyNoticeViewModel;
+
+        // Assert
+        Assert.Equal(privacyNotice.Title, viewModel.PrivacyNotice.Title);
+    }
+
+    [Fact]
+    public async Task Detail_ReturnsNotFound()
+    {
+        // Arrange
+        _processedRepository
+            .Setup(_ => _.Get<PrivacyNotice>(It.IsAny<string>(), It.IsAny<List<Query>>()))
+            .ReturnsAsync(new HttpResponse(404, "error", string.Empty));
+
+        // Act
+        StatusCodeResult result = await _privacyNoticeController.Detail("slug") as StatusCodeResult;
+
+        // Assert
+        Assert.Equal(404, result.StatusCode);
+    }
+}
