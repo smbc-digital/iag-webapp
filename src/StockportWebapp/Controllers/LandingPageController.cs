9401f58aa22e32731a4d689f139c459ce7732c38
Elena Secara <elena.secara@stockport.gov.uk>
2024-08-08 10:35:37 +0100

    chore(LandingPageController): add feature toggle

--- src/StockportWebapp/Controllers/LandingPageController.cs
+++ src/StockportWebapp/Controllers/LandingPageController.cs
@@ -4,12 +4,20 @@ namespace StockportWebapp.Controllers;
 public class LandingPageController : Controller
 {
     private readonly IRepository _repository;
+    private readonly IFeatureManager _featureManager;
 
-    public LandingPageController(IRepository repository) => _repository = repository;
+    public LandingPageController(IRepository repository, IFeatureManager featureManager = null)
+    {
+        _repository = repository;
+        _featureManager = featureManager;
+    } 
 
     [Route("/landing/{slug}")]
     public async Task<IActionResult> Index(string slug)
     {
+        if(_featureManager is not null && !_featureManager.IsEnabledAsync("LandingPages").Result)
+            return NotFound();
+        
         HttpResponse response = await _repository.Get<LandingPage>(slug);
 
         if (!response.IsSuccessful())
