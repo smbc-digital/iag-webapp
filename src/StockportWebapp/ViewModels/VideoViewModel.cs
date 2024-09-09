e1e9c9c30148421e9ebda8e1f315a7262928a5e2
Jonathan Hadley <jonathan.hadley@stockport.gov.uk>
2024-09-09 11:14:18 +0100

    patch: code cleanup

--- src/StockportWebapp/ViewModels/VideoViewModel.cs
+++ src/StockportWebapp/ViewModels/VideoViewModel.cs
@@ -1,16 +1,15 @@
-﻿namespace StockportWebapp.ViewModels
-{
-    public class VideoViewModel
-    {
-        public string VideoToken { get; set; }
-        public string PhotoId { get; set; }
-        public string Title { get; set; } = string.Empty;
+﻿namespace StockportWebapp.ViewModels;
 
-        public VideoViewModel(string title, string videoToken, string photoId)
-        {
-            Title = title;
-            VideoToken = videoToken;
-            PhotoId = photoId;
-        }
+public class VideoViewModel
+{
+    public string VideoToken { get; set; }
+    public string PhotoId { get; set; }
+    public string Title { get; set; } = string.Empty;
+
+    public VideoViewModel(string title, string videoToken, string photoId)
+    {
+        Title = title;
+        VideoToken = videoToken;
+        PhotoId = photoId;
     }
 }
