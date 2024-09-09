a8ad95748235b60c16029eae421443a53bc93c5a
Jonathan Hadley <jonathan.hadley@stockport.gov.uk>
2024-09-09 10:28:11 +0100

    minor: add video content block and partials, refactor rendering of video tag parser to use new partial as opposed to sting concat html

---
+++ src/StockportWebapp/ViewModels/VideoViewModel.cs
@@ -0,0 +1,16 @@
+﻿namespace StockportWebapp.ViewModels
+{
+    public class VideoViewModel
+    {
+        public string VideoToken { get; set; }
+        public string PhotoId { get; set; }
+        public string Title { get; set; } = string.Empty;
+
+        public VideoViewModel(string title, string videoToken, string photoId)
+        {
+            Title = title;
+            VideoToken = videoToken;
+            PhotoId = photoId;
+        }
+    }
+}
