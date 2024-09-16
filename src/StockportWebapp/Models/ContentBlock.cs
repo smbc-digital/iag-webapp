e1ecae7f43e451fbd98a838696baddb2266c94e9
Elena Secara <elena.secara@stockport.gov.uk>
2024-09-16 15:02:03 +0100

    feat(NewsBanner): add new component

--- src/StockportWebapp/Models/ContentBlock.cs
+++ src/StockportWebapp/Models/ContentBlock.cs
@@ -22,6 +22,8 @@ public class ContentBlock
     public string VideoTitle;
     public string VideoToken;
     public string VideoPlaceholderPhotoId;
+    public string AssociatedTagCategory;
+    public News NewsArticle;
     public string GetNavigationLink(string additionalUrlContent) => TypeRoutes.GetUrlFor(Type, $"{additionalUrlContent}/{Slug}");
     public string SolidBackgroundColourClass => $"bg-solid{CssClassMapper.GetBoldCssClass(ColourScheme)}"; 
     public string BackgroundColourClass => $"bg{CssClassMapper.GetCssClass(ColourScheme)}";
@@ -30,7 +32,7 @@ public class ContentBlock
     public string BorderColourClass => $"border-colour{CssClassMapper.GetBoldCssClass(ColourScheme)}";
 
     public ContentBlock(string slug, string title, string teaser, string icon, string type, string contentType, string image, string mailingListId, string body, List<ContentBlock> subItems, string link, string buttonText, EColourScheme colourScheme,
-                        string statistic, string statisticSubheading, string videoTitle, string videoToken, string videoPlaceholderPhotoId)
+                        string statistic, string statisticSubheading, string videoTitle, string videoToken, string videoPlaceholderPhotoId, string associatedTagCategory, News newsArticle)
     {
         Slug = slug;
         Title = title;
@@ -51,5 +53,7 @@ public class ContentBlock
         VideoTitle = videoTitle;
         VideoToken = videoToken;
         VideoPlaceholderPhotoId = videoPlaceholderPhotoId;
+        AssociatedTagCategory = associatedTagCategory;
+        NewsArticle = newsArticle;
     }
 }
