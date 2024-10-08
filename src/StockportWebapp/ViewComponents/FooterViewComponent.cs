d3ad2a314b0fbe78e96e7017eaa8aae4f1ce670f
Elena Secara <elena.secara@stockport.gov.uk>
2024-10-08 12:15:57 +0100

    feat(Footer): add stockroom footer + styling

--- src/StockportWebapp/ViewComponents/FooterViewComponent.cs
+++ src/StockportWebapp/ViewComponents/FooterViewComponent.cs
@@ -4,24 +4,29 @@ public class FooterViewComponent : ViewComponent
 {
     private readonly IRepository _repository;
     private readonly ILogger<FooterViewComponent> _logger;
+    private readonly MarkdownWrapper _markdownWrapper;
 
-    public FooterViewComponent(IRepository repository, ILogger<FooterViewComponent> logger)
+    public FooterViewComponent(IRepository repository, ILogger<FooterViewComponent> logger, MarkdownWrapper markdownWrapper)
     {
         _repository = repository;
         _logger = logger;
+        _markdownWrapper = markdownWrapper;
     }
 
     public async Task<IViewComponentResult> InvokeAsync()
     {
         _logger.LogInformation("Call to retrieve the footer");
 
-        var footerHttpResponse = await _repository.Get<Footer>();
+        HttpResponse footerHttpResponse = await _repository.Get<Footer>();
 
         if (!footerHttpResponse.IsSuccessful())
             return await Task.FromResult(View("NoFooterFound"));
 
-        var model = footerHttpResponse.Content as Footer;
-
+        Footer model = footerHttpResponse.Content as Footer;
+        model.FooterContent1 = _markdownWrapper.ConvertToHtml(model.FooterContent1);
+        model.FooterContent2 = _markdownWrapper.ConvertToHtml(model.FooterContent2);
+        model.FooterContent3 = _markdownWrapper.ConvertToHtml(model.FooterContent3);
+        
         return await Task.FromResult(View(model));
     }
 }
