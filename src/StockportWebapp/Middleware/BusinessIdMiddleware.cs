b413e368fb2fe9be2c62a7d494a7ea724ac2eb8b
Elena Secara <elena.secara@stockport.gov.uk>
2024-10-17 10:32:06 +0100

    feat(RelatedContent): add new component to stockroom article, wip

--- src/StockportWebapp/Middleware/BusinessIdMiddleware.cs
+++ src/StockportWebapp/Middleware/BusinessIdMiddleware.cs
@@ -22,8 +22,8 @@ public class BusinessIdMiddleware
         else
         {
             // businessId.SetId(new StringValues("healthystockport"));
-            businessId.SetId(new StringValues("stockportgov"));
-            // businessId.SetId(new StringValues("stockroom"));
+            // businessId.SetId(new StringValues("stockportgov"));
+            businessId.SetId(new StringValues("stockroom"));
             context.Request.Headers.Add("BUSINESS-ID", businessId.ToString());
             if (context.Request.Path.HasValue
                 && !context.Request.Path.Value.ToLower().Contains("/assets/")
