4d5ff10b37990f4a1772a7505aad8256b254d5fe
Elena Secara <elena.secara@stockport.gov.uk>
2024-10-17 13:59:33 +0100

    chore(Article): related content styles + images left/right

--- src/StockportWebapp/Utils/MarkdownWrapper.cs
+++ src/StockportWebapp/Utils/MarkdownWrapper.cs
@@ -1,4 +1,6 @@
-﻿namespace StockportWebapp.Utils;
+﻿using System.Net.NetworkInformation;
+
+namespace StockportWebapp.Utils;
 
 public class MarkdownWrapper
 {
@@ -9,8 +11,14 @@ public class MarkdownWrapper
     private static string Convert(string markdown)
     {
         string html = Markdown.ToHtml(markdown ?? string.Empty, new MarkdownPipelineBuilder().UsePipeTables().UseSoftlineBreakAsHardlineBreak().UseAdvancedExtensions().Build());
-        return WrapTables(html);
+        string wrappedTableHtml = WrapTables(html);
+        
+        return ReplaceFigCaptionFloat(wrappedTableHtml);
     }
 
     private static string WrapTables(string html) => html.Replace("<table>", "<div class=\"table\">\n<table>").Replace("</table>", "</table>\n</div>");
+
+    private static string ReplaceFigCaptionFloat(string html) => html
+        .Replace("<figure>\n<figcaption>#right</figcaption>", "<figure class='image-right'>")
+        .Replace("<figure>\n<figcaption>#left</figcaption>", "<figure class='image-left'>");
 }
