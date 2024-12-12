42264943a971ca428479d8e00d3385aaab3e2740
Elena Secara <elena.secara@stockport.gov.uk>
2024-12-12 09:29:00 +0000

    feat(Homepage): add featured events

--- src/StockportWebapp/ViewModels/HomepageViewModel.cs
+++ src/StockportWebapp/ViewModels/HomepageViewModel.cs
@@ -5,5 +5,6 @@ public class HomepageViewModel
     public ProcessedHomepage HomepageContent { get; set; }
     public List<Event> EventsFromApi { get; set; }
     public Event FeaturedEvent { get; set; }
+    public List<Event> FeaturedEvents { get; set; }
     public News FeaturedNews { get; set; }
 }
