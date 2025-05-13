0e43742fd86dff98faf65d9963ec0d8a4731e1b6
Elena Secara <52406019+elena-secara@users.noreply.github.com>
2025-05-13 13:57:09 +0100

    chore(News): few touch ups for margins

--- src/StockportWebapp/ViewModels/Newsroom.cs
+++ src/StockportWebapp/ViewModels/Newsroom.cs
@@ -22,7 +22,7 @@ public class Newsroom(List<News> news,
     public NavCardList LatestNews => new()
     {
         Items = News.Select(news => new NavCard(news.Title,
-                                                $"news-article/{news.Slug}",
+                                                news.Slug,
                                                 news.Teaser,
                                                 news.ThumbnailImage,
                                                 news.Image,
