b4c74e1a667473ee89e1da00abdc3b3d83193548
Elena Secara <52406019+elena-secara@users.noreply.github.com>
2025-05-28 09:29:38 +0100

    chore(TrustedLogo): rename File to Image and Url to Link

--- src/StockportWebapp/Models/TrustedLogo.cs
+++ src/StockportWebapp/Models/TrustedLogo.cs
@@ -1,13 +1,13 @@
 ﻿namespace StockportWebapp.Models;
 
 [ExcludeFromCodeCoverage]
-public class TrustedLogo(string title, string text, MediaAsset file, string url)
+public class TrustedLogo(string title, string text, MediaAsset image, string link)
 {
     public string Title { get; set; } = title;
 
     public string Text { get; set; } = text;
 
-    public MediaAsset File { get; set; } = file;
+    public MediaAsset Image { get; set; } = image;
 
-    public string Url { get; set; } = url;
+    public string Link { get; set; } = link;
 }
