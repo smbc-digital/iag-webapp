bd05ff343b165c35a9e5b9ab4901ed8db7425a2e
Elena Secara <52406019+elena-secara@users.noreply.github.com>
2025-06-19 09:57:33 +0100

    chore(ContactUsTagParser): redesign form and success page

--- src/StockportWebapp/Models/ContactUsId.cs
+++ src/StockportWebapp/Models/ContactUsId.cs
@@ -1,11 +1,10 @@
 ﻿namespace StockportWebapp.Models;
 
 [ExcludeFromCodeCoverage]
-public class ContactUsId(string name, string slug, string emailAddress, string successPageButtonText, string successPageReturnUrl)
+public class ContactUsId(string name, string slug, string emailAddress, string successPageButtonText)
 {
     public string Name { get; } = name;
     public string Slug { get; } = slug;
     public string EmailAddress { get; } = emailAddress;
     public string SuccessPageButtonText { get; } = successPageButtonText;
-    public string SuccessPageReturnUrl { get; } = successPageReturnUrl;
 }
