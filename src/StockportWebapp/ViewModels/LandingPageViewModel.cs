b3903f58e5aa811ec9505bf583688a07345e57ab
Elena Secara <52406019+elena-secara@users.noreply.github.com>
2025-07-11 06:32:53 +0100

    chore(LandingPageViewModel): remove ScreenWidth check - not used

--- src/StockportWebapp/ViewModels/LandingPageViewModel.cs
+++ src/StockportWebapp/ViewModels/LandingPageViewModel.cs
@@ -3,7 +3,4 @@
 public class LandingPageViewModel(LandingPage landingPage)
 {
     public readonly LandingPage LandingPage = landingPage;
-
-    public bool ScreenWidth =>
-        LandingPage.PageSections.Any(pageSection => pageSection is not null && pageSection.ContentType.Contains("ScreenWidth"));
 }
