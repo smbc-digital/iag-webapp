f01af5cb1bca39c9b0aa2abfe2f2e29d95e9bbfd
Elena Secara <52406019+elena-secara@users.noreply.github.com>
2025-10-06 10:49:11 +0100

    chore(Shed): fix logic to show link to historic england website

--- src/StockportWebapp/Client/ShedApiClient.cs
+++ src/StockportWebapp/Client/ShedApiClient.cs
@@ -10,17 +10,23 @@ public interface IShedApiClient
 public class ShedApiClient : IShedApiClient
 {
     private readonly System.Net.Http.HttpClient _httpClient;
+    private readonly ILogger<ShedApiClient> _logger;
 
-    public ShedApiClient(System.Net.Http.HttpClient httpClient, IApplicationConfiguration _config)
+    public ShedApiClient(System.Net.Http.HttpClient httpClient, IApplicationConfiguration _config, ILogger<ShedApiClient> logger)
     {
         _httpClient = httpClient;
         _httpClient.BaseAddress = new Uri(_config.GetShedApiBaseUrl());
+        _logger = logger;
         _httpClient.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Bearer", _config.GetShedApiAuthToken());
     }
 
     public async Task<string> GetSHEDDataByHeRef(string name)
     {
         HttpResponseMessage response = await _httpClient.GetAsync($"GetSHEDDataByHeRef?id={Uri.EscapeDataString(name)}");
+
+        if (!response.IsSuccessStatusCode)
+            _logger.LogError($"{nameof(ShedApiClient)}::{nameof(GetSHEDDataByHeRef)}: An error occurred fetching the SHED data: {response.StatusCode} - {response.ReasonPhrase}");
+
         response.EnsureSuccessStatusCode();
 
         return await response.Content.ReadAsStringAsync();
@@ -44,6 +50,10 @@ public class ShedApiClient : IShedApiClient
             url += "?" + string.Join("&", queryParams);
 
         HttpResponseMessage response = await _httpClient.GetAsync(url);
+
+        if (!response.IsSuccessStatusCode)
+            _logger.LogError($"{nameof(ShedApiClient)}::{nameof(GetSHEDDataByNameWardsAndListingTypes)}: An error occurred fetching the SHED data: {response.StatusCode} - {response.ReasonPhrase}");
+
         response.EnsureSuccessStatusCode();
 
         return await response.Content.ReadAsStringAsync();
