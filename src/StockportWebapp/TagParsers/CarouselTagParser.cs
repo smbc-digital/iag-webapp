88dc831643f2183f73432d5d6ac22b28a34fb6d9
Elena Secara <52406019+elena-secara@users.noreply.github.com>
2025-12-18 14:34:16 +0000

    chore(HomepageHeroMobile): fix search and duplicate id issue

--- src/StockportWebapp/TagParsers/CarouselTagParser.cs
+++ src/StockportWebapp/TagParsers/CarouselTagParser.cs
@@ -45,7 +45,7 @@ public class CarouselTagParser : ISimpleTagParser
             }
         }
 
-        string scriptTag = "<script>\r\nrequire(['/assets/javascript/config-7ac3d955.min.js'],function(){\r\nrequire(['slick', 'carousel'],\r\nfunction(_, carousel){\r\ncarousel.Init();\r\n}\r\n);\r\n});\r\n</script>";
+        string scriptTag = "<script>\r\nrequire(['/assets/javascript/config-c9dab664.min.js'],function(){\r\nrequire(['slick', 'carousel'],\r\nfunction(_, carousel){\r\ncarousel.Init();\r\n}\r\n);\r\n});\r\n</script>";
         
         return returnCarousel.Append("</div>" + scriptTag).ToString();
     }
