6009c6262ea9c3a04c52def80d0671a2d9048f97
Jonathan Hadley <jonathan.hadley@stockport.gov.uk>
2026-07-20 09:18:17 +0100

    patch: add alternative homepage route to fix potential cloudfront defaut route issues

--- src/StockportWebapp/Controllers/HomeController.cs
+++ src/StockportWebapp/Controllers/HomeController.cs
@@ -15,7 +15,7 @@ public class HomeController(BusinessId businessId,
     private readonly IStockportApiEventsService _stockportApiEventsService = stockportApiService;
 
     [Route("/")]
-    [Route("/")]
+    [Route("/home")]
     public async Task<IActionResult> Index()
     {
         ProcessedHomepage homepage = await _homepageService.GetHomepage();
